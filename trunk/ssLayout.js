fdfa6a0b1fc2886f4cbbdf35a05c7821659edb57
GSchizas <GSchizas@9cf8eb16-b26d-11dd-9381-bf584fb1fa83>
2008-12-17 21:49:32 +0000

    Really major: Very first edition of SigmaStorm 2 Helper! Also some serious reorg.

---
+++ trunk/ssLayout.js
@@ -0,0 +1,56 @@
+var ssLayout = {
+
+	injectMenu: function() {
+		ssLayout.injectOneMenu("Medal Guide", "index.php?cmd=profile&subcmd=medalguide", 6, "menuSource_0");
+		ssLayout.injectOneMenu("Quest Manager", "index.php?cmd=notepad&subcmd=questmanager", 7, "menuSource_0");
+		ssLayout.injectOneMenu("Inventory Manager", "index.php?cmd=notepad&subcmd=invmanager", 8, "menuSource_0");
+		ssLayout.injectOneMenu("Recipe Manager", "index.php?cmd=notepad&subcmd=recipemanager", 9, "menuSource_0");
+		ssLayout.injectOneMenu("Guild Inventory", "index.php?cmd=notepad&subcmd=guildinvmanager", 2, "menuSource_5");
+		ssLayout.injectOneMenu("Top 250 Players", "index.php?cmd=toprated&subcmd=xp", 0, "menuSource_3");
+		if (GM_getValue("keepLogs")) {
+			ssLayout.injectOneMenu("Combat Logs", "index.php?cmd=notepad&subcmd=showlogs", 10, "menuSource_0")
+		}
+	},
+
+	injectOneMenu: function(text, href, position, insertAt) {
+		var menuTable = calfSystem.findNode("//div[@id='" + insertAt + "']/table");
+		if (!menuTable) return;
+		var newRow;
+		newRow = menuTable.insertRow(position);
+		newRow.setAttribute("height", 20);
+		var newCell = newRow.insertCell(0);
+		newCell.innerHTML='&nbsp;&nbsp;&nbsp;&nbsp;&#xbb;&nbsp;&nbsp;<A href="' + href + '">' + text + '</A>';
+	},
+
+	hideBanner: function() {
+		if (!GM_getValue("hideBanner")) return;
+		var bannerElement = calfSystem.findNode("//img[(@title='Fallen Sword RPG')]");
+		if (bannerElement) bannerElement.style.display = "none";
+	},
+
+	moveFSBox: function() {
+		if (!GM_getValue("moveFSBox")) return;
+		var src=calfSystem.findNode("//b[.='FSBox']/../../../../..");
+		if (!src) return;
+		src.parentNode.removeChild(src.nextSibling);
+		var dest=calfSystem.findNode("//img[contains(@src,'menu_logout.gif')]/../../../../..");
+		// window.alert(dest);
+		var info = dest.insertRow(26);
+		var cell = info.insertCell(0);
+		cell.innerHTML="&nbsp;";
+		info = dest.insertRow(26);
+		cell = info.insertCell(0);
+		cell.setAttribute("align", "center");
+		cell.appendChild(src);
+	},
+
+	hideNewBox: function() {
+		if (!GM_getValue("hideNewBox")) return;
+		var removeThis = calfSystem.findNode("//font[b='New?']/../../../..");
+		if (!removeThis) return;
+		removeThis.parentNode.removeChild(removeThis.nextSibling);
+		removeThis.parentNode.removeChild(removeThis.nextSibling);
+		removeThis.parentNode.removeChild(removeThis);
+	},
+
+}
