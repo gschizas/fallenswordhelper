8ae762722cb52f61a46dea2a73896f76963775c5
ipodseeker <ipodseeker@9cf8eb16-b26d-11dd-9381-bf584fb1fa83>
2009-05-22 15:11:04 +0000

    SS2: Fixed minimap broken again

--- trunk/fallenswordhelper.user.js
+++ trunk/fallenswordhelper.user.js
@@ -6756,7 +6756,7 @@ var Helper = {
 		// doc = doc.replace(/<[^>]*>(<center><[^>]*title="You are here")>/g, '$1 width=11 height=11>');
 		//doc = doc.replace("<center></center>", "");
 		doc = doc.replace(/<[^>]*title="You are here"[^>]*>/g, '');
-		doc = doc.replace(/<table border="0" cellpadding="0" cellspacing="0" width="40" height="40"><tbody><tr><td[^>]*><\/td><\/tr><\/tbody><\/table>/g,'');
+		doc = doc.replace(/<table [^>]*><tbody><tr><td[^>]*><\/td><\/tr><\/tbody><\/table>/g,'');
 		doc = doc.replace(/width="40"/g, 'width="' + size + '"').replace(/height="40"/g, 'height="' + size + '"');
 		miniMap.innerHTML = doc;
 
