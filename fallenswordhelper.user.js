88f409db2e1e3dfa3070e8860c2cfde4cd3a17af
Adrian Tompkins <atompkins@gmail.com>
2015-09-18 08:30:53 +0100

    Fix QuickBuff when player has only one buff running. Tighten code. #64

--- fallenswordhelper.user.js
+++ fallenswordhelper.user.js
@@ -7242,28 +7242,25 @@ var Helper = {
 	},
 
 	addBuffLevels: function() {
-		$('span.fshSelf').remove();
-		var self = $(this);
-		Helper.addStatsQuickBuff(self);
-		var buffs = self.data('buffs').split(',');
-		var hash = {};
-		self.next().find('span').each(function(i, e) {
-			hash[buffs[i]] = $(e).text().replace(/\[|\]/g, '');
-		});
-		Object.keys(hash).forEach(function(value) {
-			var buffLvl = parseInt(hash[value], 10);
-			var label = $('label[for="skill-' + value + '"]');
+		$('span.fshPlayer').remove();
+		var player = $(this);
+		Helper.addStatsQuickBuff(player);
+		var data = player.data('buffs');
+		var buffs = typeof data === 'string' ? data.split(',') : [data];
+		player.next().find('span').each(function(i, e) {
+			var buffLvl = parseInt($(e).text().replace(/\[|\]/g, ''), 10);
+			var label = $('label[for="skill-' + buffs[i] + '"]');
 			if (label.length === 0) {return;}
 			var span = $('span > span', label);
 			var myLvl = parseInt(span.text().replace(/\[|\]/g, ''), 10);
-			span.after('<span class="fshSelf"' + (myLvl > buffLvl ?
+			span.after('<span class="fshPlayer"' + (myLvl > buffLvl ?
 				' style="color:red;"' : ' style="color:green;"') + '> [' +
 				buffLvl + ']</span>');
 		});
 	},
 
-	addStatsQuickBuff: function(self) {
-		self.parent().find('span.fshLastActivity').remove();
+	addStatsQuickBuff: function(player) {
+		player.parent().find('span.fshLastActivity').remove();
 		$.ajax({
 			cache: false,
 			dataType: 'json',
@@ -7271,11 +7268,11 @@ var Helper = {
 			data: {
 				cmd:             'export',
 				subcmd:          'profile',
-				player_username: self.text()
+				player_username: player.text()
 			},
 			success: function(data) {
 				//console.log('7344 data', data);
-				self.after('<span class="fshLastActivity">' +
+				player.after('<span class="fshLastActivity">' +
 					System.formatLastActivity(data.last_login) +
 					'<br>Stamina: ' + data.current_stamina + ' / ' +
 					data.stamina + ' ( ' + Math.floor(data.current_stamina /
