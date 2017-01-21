9b9a1e34899c0ad4f1e67d8d70939e18a0e90961
atompkins <atompkins@gmail.com>
2017-01-21 09:52:28 +0000

    DOMParser polyfill for UC Browser

--- src/calfSystem.js
+++ src/calfSystem.js
@@ -1,4 +1,5 @@
 window.FSH = window.FSH || {};
+import './modules/support/DOMParser';
 import './modules/support/performanceNow';
 import './modules/support/gmApi';
 import './modules/chrome/environment';
