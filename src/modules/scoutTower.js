10fe8206db74b442afc85c9ffde1c70466c2180e
atompkins <atompkins@gmail.com>
2017-02-18 20:30:06 +0000

    new buttons in scout tower

--- src/modules/scoutTower.js
+++ src/modules/scoutTower.js
@@ -1,28 +1,41 @@
 import * as system from './support/system';
 import * as layout from './support/layout';
 
-function injectScouttowerBuffLinks() { // Legacy
-  var titanTables = system.findNodes('//table[tbody/tr/td/font[.="Guild Member"]]');
-  var titanTable;
-  if (titanTables) {
-    for (var i = 0; i < titanTables.length; i += 1) {
-      titanTable = titanTables[i];
-      var shortList = [];
-      if (titanTable.rows.length <= 1) {continue;}
-      for (var j = 1; j < titanTable.rows.length; j += 1) {
-        if (titanTable.rows[j].cells[1]) {
-          var firstCell = titanTable.rows[j].cells[0];
-          var playerID = /player_id=(\d+)/.exec(firstCell.innerHTML)[1];
-          shortList.push(firstCell.textContent);
-          firstCell.innerHTML += ' <a style="color:blue;font-size:10px;" ' +
-            layout.quickBuffHref(playerID) + '>[b]</a>';
-        }
-      }
-      titanTable.rows[0].cells[0].innerHTML += ' <a style="color:blue;font-size:10px;">all</a>';
-      var buffAllLink = titanTable.rows[0].cells[0].firstChild.nextSibling.nextSibling;
-      buffAllLink.setAttribute('href',layout.buffAllHref(shortList));
-    }
+var titanTables;
+
+function evtHdl(e) { // Native
+  if (!e.target.classList.contains('fshBl')) {return;}
+  var self = e.target;
+  if (self.textContent === '[b]') {
+    layout.openQuickBuffByName(self.previousElementSibling.textContent);
   }
+  if (self.textContent === 'all') {
+    var titanTable = self.parentNode.parentNode.parentNode.parentNode;
+    var shortList = [];
+    for (var j = 1; j < titanTable.rows.length; j += 2) {
+      var firstCell = titanTable.rows[j].cells[0].firstChild.firstChild;
+      shortList.push(firstCell.textContent);
+    }
+    layout.openQuickBuffByName(shortList.join());
+  }
+}
+
+function injectScouttowerBuffLinks() { // Native
+  titanTables = document.getElementById('pCC')
+    .getElementsByTagName('table');
+  if (titanTables.length < 3) {return;}
+  for (var i = 2; i < titanTables.length; i += 1) {
+    var titanTable = titanTables[i];
+    if (titanTable.rows.length < 2) {continue;}
+    for (var j = 1; j < titanTable.rows.length; j += 2) {
+      var firstCell = titanTable.rows[j].cells[0];
+      firstCell.insertAdjacentHTML('beforeend',
+        ' <button class="fshBl fshXSmall">[b]</button>');
+    }
+    titanTable.rows[0].cells[0].insertAdjacentHTML('beforeend',
+      ' <button class="fshBl fshXSmall">all</button>');
+  }
+  titanTables[1].addEventListener('click', evtHdl);
 }
 
 function getScoutTowerDetails(responseText) { // Legacy
@@ -52,34 +65,30 @@ export function injectTitan() { // Legacy
 
 export function injectScouttower() { // Legacy
   injectScouttowerBuffLinks();
-  var titanTable = system.findNode('//table[@width="500"]');
-  for (var i = 1; i < titanTable.rows.length; i += 1) {
+  var titanTable = titanTables[1];
+  for (var i = 1; i < titanTable.rows.length; i += 6) {
     var aRow = titanTable.rows[i];
-    if (aRow.cells[2]) {
-      var titanHP = aRow.cells[2].textContent;
-      if (titanHP.search('-') !== -1) {break;}
-      var guildKills = aRow.cells[3].textContent;
-      if (guildKills) {
-        var titanHPArray = titanHP.split('/');
-        var currentHP = parseInt(titanHPArray[0], 10);
-        var totalHP = parseInt(titanHPArray[1], 10);
-        var currentNumberOfKills = totalHP - currentHP;
-        var numberOfKillsToSecure = Math.ceil(totalHP/2 + 1);
+    var titanHP = aRow.cells[2].textContent;
+    if (titanHP.indexOf('-') !== -1) {break;}
+    var guildKills = aRow.cells[3].textContent;
+    var titanHPArray = titanHP.split('/');
+    var currentHP = parseInt(titanHPArray[0], 10);
+    var totalHP = parseInt(titanHPArray[1], 10);
+    var currentNumberOfKills = totalHP - currentHP;
+    var numberOfKillsToSecure = Math.ceil(totalHP/2 + 1);
 
-        var titanString = '<span style="color:red;">' +
-          (numberOfKillsToSecure - guildKills) + '</span> to secure';
-        if (guildKills >= numberOfKillsToSecure) {
-          titanString = 'Secured';
-        } else if (numberOfKillsToSecure - guildKills > currentHP) {
-          titanString = '<span style="color:red;">Cannot Secure</span>';
-        }
-        var killsPercent = (currentNumberOfKills === 0 ? 0 :
-          guildKills * 100/currentNumberOfKills).toFixed(2);
-        var killsTotPct = (guildKills * 100/totalHP).toFixed(2);
-        aRow.cells[3].innerHTML += '<br><span style="color:blue;"> (' +
-          killsPercent + '% Current <br>' +
-          killsTotPct + '% Total<br>' + titanString + ')';
-      }
+    var titanString = '<span style="color:red;">' +
+      (numberOfKillsToSecure - guildKills) + '</span> to secure';
+    if (guildKills >= numberOfKillsToSecure) {
+      titanString = 'Secured';
+    } else if (numberOfKillsToSecure - guildKills > currentHP) {
+      titanString = '<span style="color:red;">Cannot Secure</span>';
     }
+    var killsPercent = (currentNumberOfKills === 0 ? 0 :
+      guildKills * 100/currentNumberOfKills).toFixed(2);
+    var killsTotPct = (guildKills * 100/totalHP).toFixed(2);
+    aRow.cells[3].innerHTML += '<br><span style="color:blue;"> (' +
+      killsPercent + '% Current <br>' +
+      killsTotPct + '% Total<br>' + titanString + ')';
   }
 }
