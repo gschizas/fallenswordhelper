f5e859828fddb0aacc94d4d9df3c3d6df0c1768f
atompkins <atompkins@gmail.com>
2017-03-14 07:36:35 +0000

    sanitise lists (quicklinks & AH quick search)

--- src/modules/lists.js
+++ src/modules/lists.js
@@ -1,136 +1,123 @@
-import calf from './support/calf';
 import * as dataObj from './support/dataObj';
 import * as layout from './support/layout';
 import * as system from './support/system';
 
+var param;
+
 function generateManageTable() { // Legacy
   var i;
   var j;
-  var result = '<table cellspacing=2 cellpadding=2 ' +
-    'style="table-layout: fixed; word-wrap: break-word;" ' +
-    'width=100%><tr bgcolor=#CD9E4B>';
-  var isArrayOnly = calf.param.fields.length === 0;
-  for (i = 0; i < calf.param.headers.length; i += 1) {
-    result += '<th>' + calf.param.headers[i] + '</th>';
+  var result = '<table cellspacing="2" cellpadding="2" class="fshGc" ' +
+    'width="100%"><tr class="fshOr">';
+  for (i = 0; i < param.headers.length; i += 1) {
+    result += '<th>' + param.headers[i] + '</th>';
   }
   result += '<th>Action</th></tr>';
   var currentCategory = '';
-  for (i = 0; i < calf.param.currentItems.length; i += 1) {
+  for (i = 0; i < param.currentItems.length; i += 1) {
     result += '<tr>';
-    if (isArrayOnly) {
-      result += '<td align=center>' + calf.param.currentItems[i] + '</td>';
-    } else {
-      if (calf.param.categoryField &&
-          currentCategory !==
-          calf.param.currentItems[i][calf.param.categoryField]) {
-        currentCategory = calf.param.currentItems[i][calf.param.categoryField];
-        result += '<td><span style="font-weight:bold; font-size:large;">' +
-          currentCategory + '</span></td></tr><tr>';
-      }
-      for (j = 0; j < calf.param.fields.length; j += 1) {
-        result += '<td align=center class=content>';
-        if (calf.param.fields[j] !== calf.param.categoryField) {
-          if (calf.param.tags[j] === 'checkbox') {
-            result += '<input type=checkbox ' +
-              (calf.param.currentItems[i][calf.param.fields[j]] ?
-              'checked' : '') + ' disabled>';
-          } else if (calf.param.url && calf.param.url[j] !== '') {
-            result += '<a href="' + calf.param.url[j].replace('@replaceme@',
-              calf.param.currentItems[i][calf.param.fields[j]]) + '">' +
-              calf.param.currentItems[i][calf.param.fields[j]] + '</a>';
-          } else {
-            result += calf.param.currentItems[i][calf.param.fields[j]];
-          }
-          result += '</td>';
+    if (param.categoryField &&
+        currentCategory !==
+        param.currentItems[i][param.categoryField]) {
+      currentCategory = param.currentItems[i][param.categoryField];
+      result += '<td><span class="fshQs">' +
+        currentCategory + '</span></td></tr><tr>';
+    }
+    for (j = 0; j < param.fields.length; j += 1) {
+      result += '<td class="fshCenter">';
+      if (param.fields[j] !== param.categoryField) {
+        if (param.tags[j] === 'checkbox') {
+          result += '<input type="checkbox" ' +
+            (param.currentItems[i][param.fields[j]] ?
+            'checked' : '') + ' disabled>';
+        } else if (param.url && param.url[j] !== '') {
+          result += '<a href="' + param.url[j].replace('@replaceme@',
+            param.currentItems[i][param.fields[j]]) + '">' +
+            param.currentItems[i][param.fields[j]] + '</a>';
+        } else {
+          result += param.currentItems[i][param.fields[j]];
         }
+        result += '</td>';
       }
     }
-    result += '<td><span class=HelperTextLink itemId="' + i +
-      '" id="Helper:DeleteItem' + i + '">[Del]</span></td></tr>';
+    result += '<td><span class="HelperTextLink" itemId="' + i +
+      '" id="fshDel' + i + '">[Del]</span></td></tr>';
   }
   result += '<tr>';
-  if (isArrayOnly) {
-    result += '<td align=center><input type=' + calf.param.tags[i] +
-      ' class=custominput id=Helper:input0></td>';
-  } else {
-    for (i = 0; i < calf.param.tags.length; i += 1) {
-      result += '<td align=center><input type=' + calf.param.tags[i] +
-        ' class=custominput id=Helper:input' + calf.param.fields[i] + '></td>';
-    }
+  for (i = 0; i < param.tags.length; i += 1) {
+    result += '<td align=center><input type="' + param.tags[i] +
+      '" class="custominput" id="fshIn' + param.fields[i] + '"></td>';
   }
-  result += '<td><span class=HelperTextLink id="Helper:AddItem">' +
-    '[Add]</span></td></tr></table>';
+  result += '<td><span class="HelperTextLink" id="fshAdd">' +
+    '[Add]</span></td></tr></table>' +
+    '<table width="100%"><tr><td class="fshCenter">' +
+    '<textarea cols=70 rows=20 name="fshEd">' +
+    JSON.stringify(param.currentItems) + '</textarea></td></tr>' +
+    '<tr><td class="fshCenter"><input id="fshSave" ' +
+    'type="button" value="Save" class="custombutton">' +
+    '&nbsp;<input id="fshReset" type="button" value="Reset" ' +
+    'class="custombutton"></td></tr>' +
+    '</tbody></table>';
 
-  if (calf.param.showRawEditor) {
-    result += '<table width=100%><tr><td align=center>' +
-      '<textarea cols=70 rows=20 name="Helper:rawEditor">' +
-      JSON.stringify(calf.param.currentItems) + '</textarea></td></tr>' +
-      '<tr><td align=center><input id="Helper:saveRawEditor" type="button" ' +
-      'value="Save" class="custombutton">' +
-      '&nbsp;<input id="Helper:resetRawEditor" type="button" value="Reset" ' +
-      'class="custombutton"></td></tr>' +
-      '</tbody></table>';
-  }
+  document.getElementById(param.id).innerHTML = result;
 
-  document.getElementById(calf.param.id).innerHTML = result;
-
-  system.setValueJSON(calf.param.gmname, calf.param.currentItems);
+  system.setValueJSON(param.gmname, param.currentItems);
 }
 
 function deleteQuickItem(evt) { // Legacy
   var itemId = evt.target.getAttribute('itemId');
-  calf.param.currentItems.splice(itemId, 1);
+  param.currentItems.splice(itemId, 1);
   generateManageTable();
 }
 
 function addQuickItem() { // Legacy
-  var isArrayOnly = calf.param.fields.length === 0;
+  var isArrayOnly = param.fields.length === 0;
   var newItem = {};
   if (isArrayOnly) {
-    newItem = document.getElementById('Helper:input0').value;
+    newItem = document.getElementById('fshIn0').value;
   } else {
-    for (var i = 0; i < calf.param.fields.length; i += 1) {
-      if (calf.param.tags[i] === 'checkbox') {
-        newItem[calf.param.fields[i]] =
-          document.getElementById('Helper:input' +
-            calf.param.fields[i]).checked;
+    for (var i = 0; i < param.fields.length; i += 1) {
+      if (param.tags[i] === 'checkbox') {
+        newItem[param.fields[i]] =
+          document.getElementById('fshIn' +
+            param.fields[i]).checked;
       } else {
-        newItem[calf.param.fields[i]] =
-          document.getElementById('Helper:input' +
-            calf.param.fields[i]).value;
+        newItem[param.fields[i]] =
+          document.getElementById('fshIn' +
+            param.fields[i]).value;
       }
     }
   }
-  calf.param.currentItems.push(newItem);
+  param.currentItems.push(newItem);
   generateManageTable();
 }
 
 function saveRawEditor() { // jQuery
-  calf.param.currentItems =
-    JSON.parse($('textarea[name="Helper:rawEditor"]').val());
+  param.currentItems =
+    JSON.parse($('textarea[name="fshEd"]').val());
   generateManageTable();
 }
 
 function resetRawEditor() { // Legacy
   if (location.search === '?cmd=notepad&blank=1&subcmd=auctionsearch') {
-    calf.param.currentItems =
+    param.currentItems =
       JSON.parse(dataObj.defaults.quickSearchList);
-  } else {calf.param.currentItems = [];}
+  } else {param.currentItems = [];}
   generateManageTable();
 }
 
 function listEvtHnl(e) { // Native
-  if (e.target.id === 'Helper:resetRawEditor') {resetRawEditor();} else
-  if (e.target.id === 'Helper:saveRawEditor') {saveRawEditor();} else
-  if (e.target.id === 'Helper:AddItem') {addQuickItem();} else
-  if (e.target.id.indexOf('Helper:DeleteItem') === 0) {deleteQuickItem(e);}
+  if (e.target.id === 'fshReset') {resetRawEditor();} else
+  if (e.target.id === 'fshSave') {saveRawEditor();} else
+  if (e.target.id === 'fshAdd') {addQuickItem();} else
+  if (e.target.id.indexOf('fshDel') === 0) {deleteQuickItem(e);}
 }
 
 export function injectAuctionSearch(injector) { // Legacy
   var content = injector || layout.pCC;
   content.innerHTML =
     layout.makePageHeader('Trade Hub Quick Search', '', '', '') +
-    '<div class=content>This screen allows you to set up some quick ' +
+    '<div>This screen allows you to set up some quick ' +
       'search templates for the Auction House. The Display on AH column ' +
       'indicates if the quick search will show on the short list on the ' +
       'Auction House main screen. A maximum of 36 items can show on this ' +
@@ -138,11 +125,11 @@ export function injectAuctionSearch(injector) { // Legacy
       'flagged). To edit items, either use the large text area below, or ' +
       'add a new entry and delete the old one. You can always reset the ' +
       'list to the default values.</div>' +
-    '<div style="font-size:small;" id="Helper:Auction Search Output">' +
+    '<div class="fshSmall" id="fshAso">' +
     '</div>';
   // global parameters for the meta function generateManageTable
-  calf.param = {
-    id: 'Helper:Auction Search Output',
+  param = {
+    id: 'fshAso',
     headers: ['Category', 'Nickname', 'Quick Search Text',
       'Display in AH?'],
     fields: ['category', 'nickname', 'searchname', 'displayOnAH'],
@@ -151,9 +138,7 @@ export function injectAuctionSearch(injector) { // Legacy
       'index.php?cmd=auctionhouse&type=-1&search_text=@replaceme@', ''],
     currentItems: system.getValueJSON('quickSearchList'),
     gmname: 'quickSearchList',
-    sortField: 'category',
     categoryField: 'category',
-    showRawEditor: true
   };
   generateManageTable();
   content.addEventListener('click', listEvtHnl);
@@ -162,19 +147,18 @@ export function injectAuctionSearch(injector) { // Legacy
 export function injectQuickLinkManager(injector) { // Legacy
   var content = injector || layout.pCC;
   content.innerHTML =
-    layout.makePageTemplate('Quick Links', '', '', '', 'quickLinkAreaId');
+    layout.makePageTemplate('Quick Links', '', '', '', 'qla');
 
   // global parameters for the meta function generateManageTable
-  calf.param = {
-    id: 'quickLinkAreaId',
+  param = {
+    id: 'qla',
     headers: ['Name', 'URL',
-      'New [<span style="cursor:pointer; text-decoration:underline;" ' +
-      'title="Open page in a new window">?</span>]'],
+      'New [<span class="fshLink tip-static" ' +
+      'data-tipped="Open page in a new window">?</span>]'],
     fields: ['name', 'url', 'newWindow'],
     tags: ['textbox', 'textbox', 'checkbox'],
     currentItems: system.getValueJSON('quickLinks'),
     gmname: 'quickLinks',
-    showRawEditor: true
   };
   generateManageTable();
   content.addEventListener('click', listEvtHnl);
