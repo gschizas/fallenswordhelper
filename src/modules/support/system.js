37b20ba14e82f219ba08d2315f1ececf50e79144
atompkins <atompkins@gmail.com>
2017-03-20 13:00:23 +0000

    fix for log message time stamp

--- src/modules/support/system.js
+++ src/modules/support/system.js
@@ -150,7 +150,7 @@ var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep',
   'Oct', 'Nov', 'Dec'];
 
 export function parseDateAsTimestamp(textDate) {
-  var dateAry = textDate.split(/[: /]/);
+  var dateAry = textDate.split(/[: /[]/);
   return Date.UTC(Number(dateAry[4]), months.indexOf(dateAry[3]),
     Number(dateAry[2]), Number(dateAry[0]), Number(dateAry[1]), 0);
 }
