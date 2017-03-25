d3148b51c47631eacd040ba2b485c340e57ff167
atompkins <atompkins@gmail.com>
2017-03-25 12:41:19 +0000

    breakout CA analysis combat analyser

--- src/modules/newMap/viewCreature.js
+++ src/modules/newMap/viewCreature.js
@@ -238,70 +238,78 @@ function evalAnalysis(combat) { // Native
   return combat;
 }
 
+function caRunning(combat) {
+  combat.lowestCALevelToStillHit = Math.max(Math.ceil((
+    combat.counterAttackBonusAttack - combat.hitByHowMuch + 1) /
+    combat.player.attackValue / 0.0025), 0);
+  combat.lowestCALevelToStillKill = Math.max(Math.ceil((
+    combat.counterAttackBonusDamage - combat.damageDone + 1) /
+    combat.player.damageValue / 0.0025), 0);
+  combat.lowestFeasibleCALevel =
+    Math.max(combat.lowestCALevelToStillHit,
+    combat.lowestCALevelToStillKill);
+  combat.extraNotes += 'Lowest CA to still 1-hit this creature = ' +
+    combat.lowestFeasibleCALevel + '<br>';
+  if (combat.lowestFeasibleCALevel !== 0) {
+    combat.extraAttackAtLowestFeasibleCALevel =
+      Math.floor(combat.player.attackValue * 0.0025 *
+      combat.lowestFeasibleCALevel);
+    combat.extraDamageAtLowestFeasibleCALevel =
+      Math.floor(combat.player.damageValue * 0.0025 *
+      combat.lowestFeasibleCALevel);
+    combat.extraNotes +=
+      'Extra CA Att/Dam at this lowered CA level = ' +
+      combat.extraAttackAtLowestFeasibleCALevel + ' / ' +
+      combat.extraDamageAtLowestFeasibleCALevel + '<br>';
+  }
+  combat.extraStaminaPerHitAtLowestFeasibleCALevel =
+    combat.player.counterAttackLevel > 0 ? Math.ceil((1 +
+    combat.player.doublerLevel / 50) * 0.0025 *
+    combat.lowestFeasibleCALevel) : 0;
+  if (combat.extraStaminaPerHitAtLowestFeasibleCALevel <
+    combat.extraStaminaPerHit) {
+    combat.extraNotes +=
+      'Extra Stam Used at this lowered CA level = ' +
+      combat.extraStaminaPerHitAtLowestFeasibleCALevel + '<br>';
+  } else {
+    combat.extraNotes +=
+      'No reduction of stam used at the lower CA level<br>';
+  }
+}
+
+function caResult(combat) {
+  combat.lowestCALevelToStillHit = Math.max(Math.ceil((
+    combat.counterAttackBonusAttack - combat.hitByHowMuch + 1) /
+    combat.player.attackValue / 0.0025), 0);
+  combat.lowestCALevelToStillKill = Math.max(Math.ceil((
+    combat.counterAttackBonusDamage - combat.damageDone + 1) /
+    combat.player.damageValue / 0.0025), 0);
+  if (combat.lowestCALevelToStillHit > 175) {
+    combat.extraNotes +=
+      'Even with CA175 you cannot hit this creature<br>';
+  } else if (combat.lowestCALevelToStillHit !== 0) {
+    combat.extraNotes += 'You need a minimum of CA' +
+      combat.lowestCALevelToStillHit +
+      ' to hit this creature<br>';
+  }
+  if (combat.lowestCALevelToStillKill > 175) {
+    combat.extraNotes +=
+      'Even with CA175 you cannot 1-hit kill this creature<br>';
+  } else if (combat.lowestCALevelToStillKill !== 0) {
+    combat.extraNotes += 'You need a minimum of CA' +
+      combat.lowestCALevelToStillKill +
+      ' to 1-hit kill this creature<br>';
+  }
+}
+
 function evalCA(combat) { // Native
   if (combat.player.counterAttackLevel > 0 &&
-    combat.numberOfHitsRequired === 1) {
-    combat.lowestCALevelToStillHit = Math.max(Math.ceil((
-      combat.counterAttackBonusAttack - combat.hitByHowMuch + 1) /
-      combat.player.attackValue / 0.0025), 0);
-    combat.lowestCALevelToStillKill = Math.max(Math.ceil((
-      combat.counterAttackBonusDamage - combat.damageDone + 1) /
-      combat.player.damageValue / 0.0025), 0);
-    combat.lowestFeasibleCALevel =
-      Math.max(combat.lowestCALevelToStillHit,
-      combat.lowestCALevelToStillKill);
-    combat.extraNotes += 'Lowest CA to still 1-hit this creature = ' +
-      combat.lowestFeasibleCALevel + '<br>';
-    if (combat.lowestFeasibleCALevel !== 0) {
-      combat.extraAttackAtLowestFeasibleCALevel =
-        Math.floor(combat.player.attackValue * 0.0025 *
-        combat.lowestFeasibleCALevel);
-      combat.extraDamageAtLowestFeasibleCALevel =
-        Math.floor(combat.player.damageValue * 0.0025 *
-        combat.lowestFeasibleCALevel);
-      combat.extraNotes +=
-        'Extra CA Att/Dam at this lowered CA level = ' +
-        combat.extraAttackAtLowestFeasibleCALevel + ' / ' +
-        combat.extraDamageAtLowestFeasibleCALevel + '<br>';
-    }
-    combat.extraStaminaPerHitAtLowestFeasibleCALevel =
-      combat.player.counterAttackLevel > 0 ? Math.ceil((1 +
-      combat.player.doublerLevel / 50) * 0.0025 *
-      combat.lowestFeasibleCALevel) : 0;
-    if (combat.extraStaminaPerHitAtLowestFeasibleCALevel <
-      combat.extraStaminaPerHit) {
-      combat.extraNotes +=
-        'Extra Stam Used at this lowered CA level = ' +
-        combat.extraStaminaPerHitAtLowestFeasibleCALevel + '<br>';
-    } else {
-      combat.extraNotes +=
-        'No reduction of stam used at the lower CA level<br>';
-    }
+      combat.numberOfHitsRequired === 1) {
+    caRunning(combat);
   }
   if (combat.numberOfHitsRequired === '-' ||
-    combat.numberOfHitsRequired !== 1) {
-    combat.lowestCALevelToStillHit = Math.max(Math.ceil((
-      combat.counterAttackBonusAttack - combat.hitByHowMuch + 1) /
-      combat.player.attackValue / 0.0025), 0);
-    combat.lowestCALevelToStillKill = Math.max(Math.ceil((
-      combat.counterAttackBonusDamage - combat.damageDone + 1) /
-      combat.player.damageValue / 0.0025), 0);
-    if (combat.lowestCALevelToStillHit > 175) {
-      combat.extraNotes +=
-        'Even with CA175 you cannot hit this creature<br>';
-    } else if (combat.lowestCALevelToStillHit !== 0) {
-      combat.extraNotes += 'You need a minimum of CA' +
-        combat.lowestCALevelToStillHit +
-        ' to hit this creature<br>';
-    }
-    if (combat.lowestCALevelToStillKill > 175) {
-      combat.extraNotes +=
-        'Even with CA175 you cannot 1-hit kill this creature<br>';
-    } else if (combat.lowestCALevelToStillKill !== 0) {
-      combat.extraNotes += 'You need a minimum of CA' +
-        combat.lowestCALevelToStillKill +
-        ' to 1-hit kill this creature<br>';
-    }
+      combat.numberOfHitsRequired !== 1) {
+    caResult(combat);
   }
   return combat;
 }
