d3017cbcd542cc5c310d23c0bd478eae10ceef32
atompkins <atompkins@gmail.com>
2017-08-21 20:38:24 +0100

    Return all arguments from AJAX

--- src/modules/ajax/retryAjax.js
+++ src/modules/ajax/retryAjax.js
@@ -1,10 +1,12 @@
 
 function doAjax(options, retries, dfr) {
-  $.ajax(options).pipe(function(data) {dfr.resolve(data);}, function(jqXhr) {
+  $.ajax(options).pipe(function(data, textStatus, jqXHR) {
+    dfr.resolve(data, textStatus, jqXHR);
+  }, function(jqXhr, textStatus, errorThrown) {
     if (retries > 0) {
       setTimeout(doAjax, 100, options, retries - 1, dfr);
     } else {
-      dfr.reject(jqXhr);
+      dfr.reject(jqXhr, textStatus, errorThrown);
     }
   });
 }
