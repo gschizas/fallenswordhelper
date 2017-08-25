760c3696c52215ba9d67d9447bbfb4002ad453e6
atompkins <atompkins@gmail.com>
2017-08-25 10:41:58 +0100

    don't refresh page for Nekid button

--- src/modules/profile/nekidBtn.js
+++ src/modules/profile/nekidBtn.js
@@ -6,22 +6,20 @@ var profileCombatSetDiv;
 function getNekid() { // jQuery
   var profileBlock = profileCombatSetDiv.nextElementSibling;
   var aLinks = profileBlock.getElementsByTagName('a');
-  var prm = [];
   Array.prototype.forEach.call(aLinks, function(link) {
     var item = /inventory_id=(\d+)/.exec(link.href)[1];
     if (item) {
-      prm.push(unequipitem(item).pipe(null, function() {return $.when();}));
+      unequipitem(item).done(function() {
+        link.parentNode.innerHTML = '';
+      });
     }
-  });
-  $.when.apply($, prm).done(function() {
-    location.assign('index.php?cmd=profile');
   });
 }
 
 export default function nekidBtn() {
   var profileRightColumn = document.getElementById('profileRightColumn');
   profileCombatSetDiv = document.getElementById('profileCombatSetDiv');
-  var targetBr = profileCombatSetDiv.parentElement.nextElementSibling;
+  var targetBr = profileCombatSetDiv.parentNode.nextElementSibling;
   var nekidDiv = createDiv({className: 'fshCenter'});
   var theBtn = createButton({
     className: 'fshBl fshBls',
