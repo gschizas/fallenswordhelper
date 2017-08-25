b4ffdf24338e347f0f36ef6c35eb6ec484289d1d
atompkins <atompkins@gmail.com>
2017-08-25 10:43:08 +0100

    limit AJAX concurrent connections

--- src/modules/ajax/retryAjax.js
+++ src/modules/ajax/retryAjax.js
@@ -1,18 +1,43 @@
+var concurrent = 0;
+var paused = true;
+var queue = [];
 
 function doAjax(options, retries, dfr) {
-  $.ajax(options).pipe(function(data, textStatus, jqXHR) {
-    dfr.resolve(data, textStatus, jqXHR);
-  }, function(jqXhr, textStatus, errorThrown) {
-    if (retries > 0) {
-      setTimeout(doAjax, 100, options, retries - 1, dfr);
-    } else {
-      dfr.reject(jqXhr, textStatus, errorThrown);
+  return $.ajax(options).pipe(dfr.resolve,
+    function(jqXhr, textStatus, errorThrown) {
+      if (retries > 0) {
+        setTimeout(doAjax, 100, options, retries - 1, dfr);
+      } else {
+        dfr.reject(jqXhr, textStatus, errorThrown);
+      }
     }
-  });
+  );
+}
+
+function taskRunner() {
+  if (queue.length === 0) {
+    paused = true;
+  } else {
+    paused = false;
+    if (concurrent < 6) {
+      concurrent += 1;
+      var opts = queue.shift();
+      doAjax.apply(null, opts).always(function() {
+        concurrent -= 1;
+        taskRunner();
+      });
+      taskRunner();
+    }
+  }
+}
+
+function add(options, retries, dfr) {
+  queue.push([options, retries, dfr]);
+  if (paused) {taskRunner();}
 }
 
 export default function retryAjax(options) {
   var dfr = $.Deferred();
-  doAjax(options, 10, dfr);
+  add(options, 10, dfr);
   return dfr;
 }
