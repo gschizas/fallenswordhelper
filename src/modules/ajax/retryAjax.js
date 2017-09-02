90fc1ce37b7417ac282f885f90ccbec2cc959868
atompkins <atompkins@gmail.com>
2017-09-02 15:57:17 +0100

    AJAX unload handler

--- src/modules/ajax/retryAjax.js
+++ src/modules/ajax/retryAjax.js
@@ -1,12 +1,28 @@
+import {mixin} from '../common/cElement';
+
 var concurrent = 0;
 var paused = true;
 var queue = [];
 
+function beforeSend(xhr) {
+  window.addEventListener('beforeunload', function() {
+    xhr.abort();
+    queue = [];
+  });
+}
+
 function doAjax(options, retries, dfr) {
-  return $.ajax(options).pipe(dfr.resolve,
+  var opt;
+  if (typeof options === 'string') {
+    opt = {url: options};
+  } else {
+    opt = options;
+  }
+  mixin(opt, {beforeSend: beforeSend});
+  return $.ajax(opt).pipe(dfr.resolve,
     function(jqXhr, textStatus, errorThrown) {
-      if (retries > 0) {
-        setTimeout(doAjax, 100, options, retries - 1, dfr);
+      if (retries > 0 && jqXhr.status === 503) {
+        setTimeout(doAjax, 100, opt, retries - 1, dfr);
       } else {
         dfr.reject(jqXhr, textStatus, errorThrown);
       }
@@ -19,7 +35,7 @@ function taskRunner() {
     paused = true;
   } else {
     paused = false;
-    if (concurrent < 6) {
+    if (concurrent < 4) {
       concurrent += 1;
       var opts = queue.shift();
       doAjax.apply(null, opts).always(function() {
