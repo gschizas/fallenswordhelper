974deeb442afd743062fca2c15fcc6cdb2cef142
atompkins <atompkins@gmail.com>
2017-11-19 23:21:02 +0000

    clean

--- src/modules/common/getElement.js
+++ src/modules/common/getElement.js
@@ -1,7 +1,4 @@
 export function getElementById(id, doc) {
-  //#if _DEV  //  Testing getElementById
-  // console.log('typeof doc', typeof doc); // eslint-disable-line no-console
-  //#endif
   if (doc) {return doc.getElementById(id);}
   return document.getElementById(id);
 }
