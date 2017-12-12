c647587e25e00c89e6a59512ac650e9dbc04cfbe
atompkins <atompkins@gmail.com>
2017-12-12 11:37:49 +0000

    try to improve upgrades page especially around flash sales

--- src/modules/upgrades.js
+++ src/modules/upgrades.js
@@ -1,66 +1,119 @@
-import {findNode, intValue, server, setValue} from './support/system';
+import {createSpan} from './common/cElement';
+import {getElementById} from './common/getElement';
+import insertElement from './common/insertElement';
+import insertTextBeforeEnd from './common/insertTextBeforeEnd';
+import isNaN from './common/isNaN';
+import {intValue, server, setValue} from './support/system';
 
+var upgrades;
 var currentFSP;
+var warehouse = {};
 
-function updateStamCount(evt) { // jQuery
-  var target = $(evt.target);
-  var amount = target.attr('amount');
-  var cost = target.attr('cost');
-  var quantity = target.val();
+function findText(text) {
+  return Array.prototype.find.call(upgrades, function(el) {
+    return el.textContent.indexOf(text) !== -1;
+  });
+}
+
+function getInputCell(label) {
+  return findText(label).nextElementSibling.nextElementSibling
+    .nextElementSibling;
+}
+
+function getInputElement(el) {
+  return el.nextElementSibling.nextElementSibling
+    .nextElementSibling.children[0].rows[0].cells[0].children[0];
+}
+
+function getRe(type, label) {
+  if (label === 'amount') {
+    return new RegExp('\\+(\\d+) ' + type);
+  }
+  return /(\d+)\xA0/;
+}
+
+function getValue(type, element, label) {
+  if (!warehouse[type][label]) {
+    var valRe = getRe(type, label);
+    var value = element.textContent.match(valRe)[1];
+    warehouse[type][label] = value;
+  }
+  return warehouse[type][label];
+}
+
+function getAmount(type, upgrade) {
+  return getValue(type, upgrade, 'amount');
+}
+
+function getCost(type, upgrade) {
+  return getValue(type, upgrade.nextElementSibling, 'cost');
+}
+
+function getCell(type, upgrade) {
+  if (!warehouse[type]) {warehouse[type] = {};}
+  if (!warehouse[type].span) {
+    var span = createSpan();
+    insertTextBeforeEnd(upgrade, ' ');
+    insertElement(upgrade, span);
+    warehouse[type].span = span;
+  }
+  return warehouse[type].span;
+}
+
+function doStamCount(type, upgrade, quantity, cell) {
+  var amount = getAmount(type, upgrade);
+  var cost = getCost(type, upgrade);
   // cap the value if the user goes over his current FSP
-  var color = 'red';
-  var extraStam = Math.floor(currentFSP / cost) * amount;
+  var extraStam;
   if (quantity * cost <= currentFSP) {
     extraStam = quantity * amount;
-    color = 'blue';
+    cell.className = 'fshBlue';
+  } else {
+    extraStam = Math.floor(currentFSP / cost) * amount;
+    cell.className = 'fshRed';
   }
-  $('#pCC span[id="totalStam"][type="' + target.attr('stamtype') + '"]')
-    .css('color', color)
-    .html('(+' + extraStam + ' stamina)');
+  cell.textContent = '(+' + extraStam + ' stamina)';
 }
 
-function injectUpgradeHelper(value, type) { // jQuery
-  var theCells = $('#pCC tr')
-    .has('input[name="upgrade_id"][value="' + value + '"]')
-    .find('td');
-  var cell = theCells.first();
-  cell.append(' <span style="color:blue" ' +
-    'id="totalStam" type="' + type + '"></span>');
-  var amountRE = new RegExp('\\+(\\d+) ' + type + ' Stamina');
-  var amount = cell.text().match(amountRE)[1];
-  $('input[name="quantity"]', theCells)
-    .attr('stamtype', type)
-    .attr('amount', amount)
-    .attr('cost', theCells.eq(1).text())
-    .keyup(updateStamCount);
+function updateStamCount(type, upgrade, evt) {
+  var self = evt.target;
+  var quantity = Number(self.value);
+  var cell = getCell(type, upgrade);
+  if (isNaN(quantity) || quantity === 0) {
+    cell.className = 'fshHide';
+    return;
+  }
+  doStamCount(type, upgrade, quantity, cell);
 }
 
-function injectPoints() { // jQuery
-  currentFSP = intValue($('#statbar-fsp').text());
-  injectUpgradeHelper(0, 'Current');
-  injectUpgradeHelper(1, 'Maximum');
-  $('#pCC td')
-    .has('input[name="upgrade_id"][value="3"]')
-    .html('<a href="' + server +
-      '?cmd=marketplace">Sell at Marketplace</a>');
+function injectUpgradeHelper(type) {
+  var upgrade = findText(type);
+  getInputElement(upgrade).addEventListener('keyup',
+    updateStamCount.bind(null, type, upgrade));
 }
 
-export default function storePlayerUpgrades() { // Legacy
-  var alliesText = findNode('//td[.="+1 Max Allies"]');
-  var alliesRatio = alliesText.nextSibling.nextSibling.nextSibling
-    .nextSibling;
-  if (alliesRatio) {
-    var alliesValueRE = /(\d+) \/ 115/;
-    var alliesValue = Number(alliesValueRE.exec(alliesRatio.innerHTML)[1]);
-    setValue('alliestotal', alliesValue + 5);
+function injectPoints() {
+  currentFSP = intValue(getElementById('statbar-fsp').textContent);
+  injectUpgradeHelper('Current');
+  injectUpgradeHelper('Maximum');
+  getInputCell('Gold').innerHTML = '<a href="' + server +
+    'index.php?cmd=marketplace">Sell at Marketplace</a>';
+}
+
+function saveUpgradeValue(upgrade, key) {
+  var text = findText(upgrade);
+  var ratio = text.nextElementSibling.nextElementSibling;
+  if (ratio) {
+    var valueRE = /(\d+) \/ 115/;
+    var value = Number(valueRE.exec(ratio.innerHTML)[1]);
+    setValue(key, value + 5);
   }
-  var enemiesText = findNode('//td[.="+1 Max Enemies"]');
-  var enemiesRatio = enemiesText.nextSibling.nextSibling.nextSibling
-    .nextSibling;
-  if (enemiesRatio) {
-    var enemiesValueRE = /(\d+) \/ 115/;
-    var enemiesValue = Number(enemiesValueRE.exec(enemiesRatio.innerHTML)[1]);
-    setValue('enemiestotal', enemiesValue + 5);
-  }
+}
+
+export default function storePlayerUpgrades() {
+  upgrades = document.querySelectorAll('#pCC > table:last-of-type > tbody > ' +
+    'tr:nth-child(even) > td:first-child');
+  saveUpgradeValue('+1 Max Allies', 'alliestotal');
+  saveUpgradeValue('+1 Max Enemies', 'enemiestotal');
   injectPoints();
 }
