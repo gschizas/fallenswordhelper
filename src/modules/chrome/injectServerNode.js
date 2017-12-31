44ccbf9ba9b1a937fabc5d4d880550162a3c906d
atompkins <atompkins@gmail.com>
2017-12-31 10:53:31 +0000

    move game stats to top banner if available

--- src/modules/chrome/injectServerNode.js
+++ src/modules/chrome/injectServerNode.js
@@ -1,6 +1,23 @@
 import {createDiv} from '../common/cElement';
 import {getElementById} from '../common/getElement';
 import insertElement from '../common/insertElement';
+import toggleForce from '../common/toggleForce';
+
+function doServerNode(topbannerStats, miniboxList) {
+  var nodeName = miniboxList.children[7].textContent;
+  var serverDiv = createDiv({
+    className: 'tip-static',
+    dataset: {tipped: 'Server'},
+    textContent: 'Server: ' + nodeName
+  });
+  insertElement(topbannerStats, serverDiv);
+}
+
+function doOnlinePlayers(topbannerStats, miniboxList) {
+  var playersOnline = miniboxList.children[3].innerHTML;
+  var bannerPlayers = topbannerStats.children[0];
+  bannerPlayers.innerHTML = 'Online: ' + playersOnline;
+}
 
 export default function injectServerNode() {
   var topbannerStats = getElementById('topbanner-stats');
@@ -11,13 +28,9 @@ export default function injectServerNode() {
   if (topbannerStats && gameStats) {
     var miniboxList = gameStats.nextElementSibling.children[0];
     if (miniboxList.children.length === 8) {
-      var nodeName = miniboxList.children[7].textContent;
-      var serverDiv = createDiv({
-        className: 'tip-static',
-        dataset: {tipped: 'Server'},
-        textContent: 'Server: ' + nodeName
-      });
-      insertElement(topbannerStats, serverDiv);
+      doServerNode(topbannerStats, miniboxList);
+      doOnlinePlayers(topbannerStats, miniboxList);
+      toggleForce(gameStats.parentNode, true);
     }
   }
 }
