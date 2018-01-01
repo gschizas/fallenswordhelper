7a3e7acd7a6e0fb5b238ad3e8c32e17ee3eb922a
atompkins <atompkins@gmail.com>
2018-01-01 23:58:56 +0000

    reduce complexity (updated eslint rule 4.14.0)

--- src/modules/newMap/monsterLog/interceptMouseEvents.js
+++ src/modules/newMap/monsterLog/interceptMouseEvents.js
@@ -1,29 +1,36 @@
 import {getElementById} from '../../common/getElement';
 
-export function interceptMouseEvents() {
-  getElementById('actionList').addEventListener('mouseover', function(evt) {
-    var self = evt.target;
-    if (self.classList.contains('verb') &&
-        self.classList.contains('view') &&
-        self.classList.contains('tip-static') &&
-        !self.classList.contains('fshTip')) {
-      self.classList.add('fshTip');
-      $(self).qtip({
-        overwrite: true,
-        show: {
-          event: evt.type,
-          ready: true
-        },
-        style: {classes: 'qtip-tipsy qtip-custom'},
-        position: {
-          my: 'center right',
-          at: 'center left',
-          effect: false,
-          viewport: $(window)
-        },
-        content: 'This is a test',
-        hide: {effect: false}
-      });
-    }
+var creatureViewTests = ['verb', 'view', 'tip-static', 'fshTip'];
+
+function isCreatureView(self) {
+  return creatureViewTests.every(function(e) {
+    return self.classList.contains(e);
   });
+}
+
+function moEvt(evt) {
+  var self = evt.target;
+  if (isCreatureView(self)) {
+    self.classList.add('fshTip');
+    $(self).qtip({
+      overwrite: true,
+      show: {
+        event: evt.type,
+        ready: true
+      },
+      style: {classes: 'qtip-tipsy qtip-custom'},
+      position: {
+        my: 'center right',
+        at: 'center left',
+        effect: false,
+        viewport: $(window)
+      },
+      content: 'This is a test',
+      hide: {effect: false}
+    });
+  }
+}
+
+export function interceptMouseEvents() {
+  getElementById('actionList').addEventListener('mouseover', moEvt);
 }
