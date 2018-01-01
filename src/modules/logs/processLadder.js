32f4c86dad5a020ad9875f0879fe7c86b90b561c
atompkins <atompkins@gmail.com>
2018-01-01 23:34:47 +0000

    reduce complexity (updated eslint rule 4.14.0)

--- src/modules/logs/processLadder.js
+++ src/modules/logs/processLadder.js
@@ -1,15 +1,22 @@
 import calf from '../support/calf';
 import {parseDateAsTimestamp, setValue} from '../support/system';
 
+function isLadderReset(aRow) {
+  return aRow.cells[2].firstElementChild &&
+    aRow.cells[2].firstElementChild.tagName === 'IMG' &&
+    aRow.cells[2].firstElementChild.src.indexOf('pvp_icon.gif') !== -1;
+}
+
+function saveLastResetTime(aRow) {
+  var logTime = parseDateAsTimestamp(aRow.cells[1].textContent);
+  if (logTime > calf.lastLadderReset) {
+    setValue('lastLadderReset', logTime);
+    calf.lastLadderReset = logTime;
+  }
+}
+
 export default function processLadder(aRow, messageType) {
-  if (messageType === 'Notification' &&
-      aRow.cells[2].firstElementChild &&
-      aRow.cells[2].firstElementChild.tagName === 'IMG' &&
-      aRow.cells[2].firstElementChild.src.indexOf('pvp_icon.gif') !== -1) {
-    var logTime = parseDateAsTimestamp(aRow.cells[1].textContent);
-    if (logTime > calf.lastLadderReset) {
-      setValue('lastLadderReset', logTime);
-      calf.lastLadderReset = logTime;
-    }
+  if (messageType === 'Notification' && isLadderReset(aRow)) {
+    saveLastResetTime(aRow);
   }
 }
