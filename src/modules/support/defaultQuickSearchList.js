7e17a09d1289d1c94fd24e7f919901340e2c7394
atompkins <atompkins@gmail.com>
2018-01-06 19:31:12 +0000

    move inv defaults to assets

---
+++ src/modules/support/defaultQuickSearchList.js
@@ -0,0 +1,49 @@
+export var quickSearchList =
+  '[{"category":"Plants","searchname":"Amber","nickname":""},' +
+  '{"category":"Plants","searchname":"Blood Bloom","nickname":""},' +
+  '{"category":"Plants","searchname":"Jademare","nickname":""},' +
+  '{"category":"Plants","searchname":"Dark Shade","nickname":""},' +
+  '{"category":"Plants","searchname":"Trinettle","nickname":""},' +
+  '{"category":"Plants","searchname":"Heffle Wart","nickname":""},' +
+  '{"category":"Potions","searchname":"Sludge Brew",' +
+    '"nickname":"DC 200","displayOnAH":true},' +
+  '{"category":"Potions","searchname":"Potion of Black Death",' +
+    '"nickname":"DC 225","displayOnAH":true},' +
+  '{"category":"Potions","searchname":"Potion of Aid",' +
+    '"nickname":"Assist","displayOnAH":true},' +
+  '{"category":"Potions","searchname":"Potion of Supreme Doubling",' +
+    '"nickname":"DB 450","displayOnAH":true},' +
+  '{"category":"Potions","searchname":"Potion of Acceleration",' +
+    '"nickname":"DB 500","displayOnAH":true},' +
+  '{"category":"Potions","searchname":"Potion of Lesser Death Dealer",' +
+    '"nickname":"DD","displayOnAH":true},' +
+  '{"category":"Potions","searchname":"Runic Potion",' +
+    '"nickname":"FI 250","displayOnAH":true},' +
+  '{"category":"Potions","searchname":"Potion of the Bookworm",' +
+    '"nickname":"Lib 225","displayOnAH":true},' +
+  '{"category":"Potions","searchname":"Potion of Truth",' +
+    '"nickname":"EW 1k","displayOnAH":true},' +
+  '{"category":"Potions","searchname":"Dull Edge",' +
+    '"nickname":"DE 25","displayOnAH":true},' +
+  '{"category":"Potions","searchname":"Notched Blade",' +
+    '"nickname":"DE 80","displayOnAH":true},' +
+  '{"category":"Potions","searchname":"Potion of Death",' +
+    '"nickname":"DW 125","displayOnAH":true},' +
+  '{"category":"Potions","searchname":"Potion of Decay",' +
+    '"nickname":"WI 150","displayOnAH":true},' +
+  '{"category":"Potions","searchname":"Potion of Fatality",' +
+    '"nickname":"WI 350","displayOnAH":true},' +
+  '{"category":"Potions","searchname":"Potion of Annihilation",' +
+    '"nickname":"DW 150","displayOnAH":true},' +
+  '{"category":"Potions","searchname":"Potion of the Wise",' +
+    '"nickname":"Lib 200","displayOnAH":true},' +
+  '{"category":"Potions","searchname":"Potion of Shattering",' +
+    '"nickname":"SA","displayOnAH":true},' +
+  '{"category":"Potions","searchname":"Dragons Blood Potion",' +
+    '"nickname":"ZK 200","displayOnAH":true},' +
+  '{"category":"Potions","searchname":"Berserkers Potion",' +
+    '"nickname":"ZK 300","displayOnAH":true},' +
+  '{"category":"Potions","searchname":"Potion of Fury",' +
+    '"nickname":"ZK 350","displayOnAH":true},' +
+  '{"category":"Potions","searchname":"Potion of Supreme Luck",' +
+    '"nickname":"FI 1k","displayOnAH":true}]';
