a0f984f99a428cb00be16fc765fc4e939046b24a
atompkins <atompkins@gmail.com>
2018-01-07 18:52:33 +0000

    Titan Log - substitute Scout Tower with link

--- src/modules/scoutTower/injectTitan.js
+++ src/modules/scoutTower/injectTitan.js
@@ -1,30 +1,10 @@
-import {createDocument} from '../support/system';
-import {getElementById} from '../common/getElement';
-import injectScouttowerBuffLinks from './injectScouttowerBuffLinks';
 import {pCC} from '../support/layout';
-import retryAjax from '../ajax/retryAjax';
 
-function insertBr(el) {
-  el.insertAdjacentHTML('beforeend', '<br><br>');
-}
-
-function getScoutTowerDetails(responseText) {
-  var doc = createDocument(responseText);
-  var scoutPcc = getElementById('pCC', doc);
-  injectScouttowerBuffLinks(scoutPcc.getElementsByTagName('table'));
-  var scoutTowerTable = scoutPcc.children[0];
-  if (scoutTowerTable) {
-    var titanTable = pCC.children[0];
-    var newRow = titanTable.insertRow(-1);
-    insertBr(newRow);
-    newRow = titanTable.insertRow(-1);
-    newRow.appendChild(scoutTowerTable.rows[1].cells[0])
-      .insertAdjacentHTML('beforeend', '<br><br>');
-    newRow = titanTable.insertRow(-1);
-    newRow.appendChild(scoutTowerTable.rows[8].cells[0]);
-  }
-}
-
-export default function injectTitan() { // jQuery.min
-  retryAjax('index.php?cmd=guild&subcmd=scouttower').done(getScoutTowerDetails);
+export default function injectTitan() {
+  var titanTable = pCC.children[0];
+  var newRow = titanTable.insertRow(2);
+  newRow.insertAdjacentHTML('beforeend', '<br>');
+  newRow = titanTable.insertRow(3);
+  newRow.insertAdjacentHTML('beforeend', '<td class="fshCenter fshBold">[ ' +
+    '<a href="index.php?cmd=guild&subcmd=scouttower">Scout Tower</a> ]</td>');
 }
