38e32230da6c399bb282a0363244e935139e0e8d
atompkins <atompkins@gmail.com>
2018-01-25 10:03:06 +0000

    refactor default quick search list

--- src/modules/support/defaultQuickSearchList.js
+++ src/modules/support/defaultQuickSearchList.js
@@ -1,40 +1,43 @@
+var thePlants = [
+  'Amber',
+  'Blood Bloom',
+  'Jademare',
+  'Dark Shade',
+  'Trinettle',
+  'Heffle Wart'
+];
+var thePotions = [
+  ['Sludge Brew', 'DC 200'],
+  ['Potion of Black Death', 'DC 225'],
+  ['Potion of Aid', 'Assist'],
+  ['Potion of Supreme Doubling', 'DB 450'],
+  ['Potion of Acceleration', 'DB 500'],
+  ['Potion of Lesser Death Dealer', 'DD'],
+  ['Runic Potion', 'FI 250'],
+  ['Potion of the Bookworm', 'Lib 225'],
+  ['Potion of Truth', 'EW 1k'],
+  ['Dull Edge', 'DE 25'],
+  ['Notched Blade', 'DE 80'],
+  ['Potion of Death', 'DW 125'],
+  ['Potion of Decay', 'WI 150'],
+  ['Potion of Fatality', 'WI 350'],
+  ['Potion of Annihilation', 'DW 150'],
+  ['Potion of the Wise', 'Lib 200'],
+  ['Potion of Shattering', 'SA'],
+  ['Dragons Blood Potion', 'ZK 200'],
+  ['Berserkers Potion', 'ZK 300'],
+  ['Potion of Fury', 'ZK 350'],
+  ['Potion of Supreme Luck', 'FI 1k']
+];
+
 function plants() {
-  return [
-    'Amber',
-    'Blood Bloom',
-    'Jademare',
-    'Dark Shade',
-    'Trinettle',
-    'Heffle Wart'
-  ].map(function(el) {
+  return thePlants.map(function(el) {
     return {category: 'Plants', searchname: el, nickname: ''};
   });
 }
 
 function potions() {
-  return [
-    ['Sludge Brew', 'DC 200'],
-    ['Potion of Black Death', 'DC 225'],
-    ['Potion of Aid', 'Assist'],
-    ['Potion of Supreme Doubling', 'DB 450'],
-    ['Potion of Acceleration', 'DB 500'],
-    ['Potion of Lesser Death Dealer', 'DD'],
-    ['Runic Potion', 'FI 250'],
-    ['Potion of the Bookworm', 'Lib 225'],
-    ['Potion of Truth', 'EW 1k'],
-    ['Dull Edge', 'DE 25'],
-    ['Notched Blade', 'DE 80'],
-    ['Potion of Death', 'DW 125'],
-    ['Potion of Decay', 'WI 150'],
-    ['Potion of Fatality', 'WI 350'],
-    ['Potion of Annihilation', 'DW 150'],
-    ['Potion of the Wise', 'Lib 200'],
-    ['Potion of Shattering', 'SA'],
-    ['Dragons Blood Potion', 'ZK 200'],
-    ['Berserkers Potion', 'ZK 300'],
-    ['Potion of Fury', 'ZK 350'],
-    ['Potion of Supreme Luck', 'FI 1k']
-  ].map(function(el) {
+  return thePotions.map(function(el) {
     return {
       category: 'Potions',
       searchname: el[0],
