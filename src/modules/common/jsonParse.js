7bf14f0e3a8565a504ee4abe523d8041de9d9c81
atompkins <atompkins@gmail.com>
2018-02-02 22:37:09 +0000

    safer jsonParse for system with reviver

--- src/modules/common/jsonParse.js
+++ src/modules/common/jsonParse.js
@@ -1,7 +1,7 @@
 
-export default function jsonParse(str) {
+export default function jsonParse(str, reviver) {
   try {
-    return JSON.parse(str);
+    return JSON.parse(str, reviver);
   } catch (e) {
     // Ignore bad json
   }
