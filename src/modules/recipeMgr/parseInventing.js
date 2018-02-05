2c9df52f07ef5389d0bd59e49a2f9a027bc5dbd0
atompkins <atompkins@gmail.com>
2018-02-05 00:03:20 +0000

    break up recipe manager

---
+++ src/modules/recipeMgr/parseInventing.js
@@ -0,0 +1,47 @@
+import {content} from './recipeMgr';
+import {createDiv} from '../common/cElement';
+import generateRecipeTable from './generateRecipeTable';
+import processFirstPage from './processFirstPage';
+import retryAjax from '../ajax/retryAjax';
+import setForage from '../ajax/setForage';
+
+export var recipebook;
+export var output;
+
+export function storeRecipeBook() {
+  setForage('fsh_recipeBook', recipebook);
+}
+
+function displayStuff() {
+  output.insertAdjacentHTML('beforeend', 'Finished parsing ... formatting ...');
+  storeRecipeBook();
+  generateRecipeTable();
+}
+
+export function parseInventingStart() { // jQuery.min
+  recipebook = {};
+  recipebook.recipe = [];
+  output.innerHTML = '<br>Parsing inventing screen ...<br>';
+  retryAjax('index.php?no_mobile=1&cmd=inventing').pipe(processFirstPage)
+    .done(displayStuff);
+}
+
+export function gotRecipeBook(data) {
+  recipebook = data;
+  // if (getValue('hideRecipes')) {
+  //   hideRecipes = shouldBeArray('hideRecipeNames');
+  // }
+  content.innerHTML = '<table class="fshInvFilter"><thead><tr>' +
+    '<th width="90%"><b>&nbsp;Recipe Manager</b></th>' +
+    '<th width="10%" class="fshBtnBox">[' +
+    '<span id="rfsh" class="fshLink">' +
+    'Refresh</span>]</th>' +
+    '</tr></thead></table>';
+  output = createDiv();
+  content.insertAdjacentElement('beforeend', output);
+  if (!recipebook) {
+    parseInventingStart();
+  } else {
+    generateRecipeTable();
+  }
+}
