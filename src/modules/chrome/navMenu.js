fa1c79e00083c6060a6787a21c595c040f4cd76a
atompkins <atompkins@gmail.com>
2018-02-12 08:05:23 +0000

    break out navMenu from lookForHcsData and test for jQuery

---
+++ src/modules/chrome/navMenu.js
@@ -0,0 +1,12 @@
+export default function navMenu() { // jQuery
+  if (!$) {return;}
+  var myNav = $('#nav').data('nav');
+  if (!myNav) {return;}
+  var oldSave = myNav._saveState;
+  myNav._saveState = function(_id) {
+    var id = _id;
+    var myHeight = $('li.nav-level-0', '#nav').eq(id).find('ul').height();
+    if (myHeight === 0) {id = -1;}
+    oldSave.call(myNav, id);
+  };
+}
