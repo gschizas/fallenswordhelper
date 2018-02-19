339b0c0133c67aef730db99b8d325713dcd7cc31
atompkins <atompkins@gmail.com>
2018-02-19 09:45:55 +0000

    another try to stringify errors

--- src/modules/common/stringifyError.js
+++ src/modules/common/stringifyError.js
@@ -1,3 +1,7 @@
 export default function stringifyError(err) {
-  return JSON.stringify(err, Object.getOwnPropertyNames(err), '|');
+  var plainObject = {};
+  Object.getOwnPropertyNames(err).forEach(function(key) {
+    plainObject[key] = err[key];
+  });
+  return JSON.stringify(plainObject);
 }
