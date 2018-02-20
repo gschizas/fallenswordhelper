2fdbe38e4a1b15173b0afec7fe57b13f966a24ef
atompkins <atompkins@gmail.com>
2018-02-20 08:58:17 +0000

    Build 15.22.35

--- dist/beta/fallenswordhelper.user.js
+++ dist/beta/fallenswordhelper.user.js
@@ -11,7 +11,7 @@
 // @exclude        https://wiki.fallensword.com/*
 // @exclude        https://www.fallensword.com/app.php*
 // @exclude        https://www.fallensword.com/fetchdata.php*
-// @version        1522b34
+// @version        1522b35
 // @downloadURL    https://fallenswordhelper.github.io/fallenswordhelper/Releases/Beta/fallenswordhelper.user.js
 // @grant          none
 // ==/UserScript==
@@ -75,7 +75,7 @@ var verTest = [
 ];
 
 function setVer() {
-  var ver = '1522b34';
+  var ver = '1522b35';
   return verTest.find(function(e) {return e[0]();})[1](ver);
 }
 
