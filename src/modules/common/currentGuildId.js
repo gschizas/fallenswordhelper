cea7f6f413614e1925c1d3dcd561f4a78036823f
atompkins <atompkins@gmail.com>
2018-03-11 09:03:14 +0000

    cache common variables

--- src/modules/common/currentGuildId.js
+++ src/modules/common/currentGuildId.js
@@ -1,9 +1,12 @@
+var guildId;
+
 export default function currentGuildId() {
-  var guildId;
-  var nodeList = document.body.getElementsByTagName('script');
-  Array.prototype.forEach.call(nodeList, function getGuildId(el) {
-    var match = el.textContent.match(/\s+guildId: ([0-9]+),/);
-    if (match) {guildId = parseInt(match[1], 10);}
-  });
+  if (!guildId) {
+    var nodeList = document.body.getElementsByTagName('script');
+    Array.prototype.forEach.call(nodeList, function getGuildId(el) {
+      var match = el.textContent.match(/\s+guildId: ([0-9]+),/);
+      if (match) {guildId = Number(match[1]);}
+    });
+  }
   return guildId;
 }
