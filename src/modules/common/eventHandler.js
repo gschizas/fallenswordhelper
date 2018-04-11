45a3a4c95a97c1733deff328d261b5f5f65c38d1
atompkins <atompkins@gmail.com>
2018-04-11 01:26:57 +0100

    clean up event handler

--- src/modules/common/eventHandler.js
+++ src/modules/common/eventHandler.js
@@ -1,12 +1,7 @@
 export default function eventHandler(evtAry) {
   return function(evt) {
     var self = evt.target;
-    evtAry.some(function(el) {
-      if (el.test(self)) {
-        el.act(self);
-        return true;
-      }
-      return false;
-    });
+    var hdl = evtAry.find(function(el) {return el.test(self);});
+    if (hdl) {hdl.act(self);}
   };
 }
