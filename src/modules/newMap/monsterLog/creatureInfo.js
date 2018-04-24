8effc73547df02b5ee8550b289c3013e227ef8e2
atompkins <atompkins@gmail.com>
2018-04-24 10:24:31 +0100

    breakup creature mouseover

--- src/modules/newMap/monsterLog/creatureInfo.js
+++ src/modules/newMap/monsterLog/creatureInfo.js
@@ -33,52 +33,86 @@ export function getMyStats() {
   statHp = getStatText(statTooltip, 'stat-hp');
 }
 
+function tipHeader(creature) {
+  return '<table><tr><td>' +
+    '<img src="https://cdn.fallensword.com/creatures/' + creature.image_id +
+    '.jpg" height="200" width="200"></td><td rowspan="2">' +
+    '<table width="400"><tr>' +
+    '<td class="header" colspan="4" class="fshCenter">Statistics</td></tr>';
+}
+
+function tipClassLevel(creature, myLvlClas) {
+  return '<tr><td>Class:&nbsp;</td><td width="40%">' + creature.creature_class +
+    '</td><td>Level:&nbsp;</td><td width="40%">' + creature.level +
+    ' (your level:<span class="' + myLvlClas + '">' + statLevel +
+    '</span>)</td></tr>';
+}
+
+function tipAttackDefense(creature) {
+  return '<tr><td>Attack:&nbsp;</td><td width="40%">' + creature.attack +
+    ' (your defense:<span class="fshYellow">' + statDefense +
+    '</span>)</td><td>Defense:&nbsp;</td><td width="40%">' + creature.defense +
+    ' (your attack:<span class="fshYellow">' + statAttack +
+    '</span>)</td></tr>';
+}
+
+function tipArmorDamage(creature) {
+  return '<tr><td>Armor:&nbsp;</td><td width="40%">' + creature.armor +
+    ' (your damage:<span class="fshYellow">' + statDamage +
+    '</span>)</td><td>Damage:&nbsp;</td><td width="40%">' + creature.damage +
+    ' (your armor:<span class="fshYellow">' + statArmor + '</span>)</td></tr>';
+}
+
+function tipHp(creature, oneHitNumber) {
+  return '<tr><td>HP:&nbsp;</td><td width="40%">' + creature.hp +
+    ' (your HP:<span class="fshYellow">' + statHp +
+    '</span>)(1H: <span class="fshRed">' + oneHitNumber +
+    '</span>)</td><td>Gold:&nbsp;</td><td width="40%">' + creature.gold +
+    '</td></tr>';
+}
+
+var tipSpacer = '<tr><td colspan="4" height="5"></td></tr><tr>' +
+  '<td class="header" colspan="4" class="fshCenter">Enhancements</td></tr>';
+
+function tipEnhancements(creature) {
+  var ret = '';
+  if (creature.enhancements.length === 0) {
+    ret += '<tr><td colspan="4">[no enhancements]</td></tr>';
+  } else {
+    creature.enhancements.forEach(function(e) {
+      ret += '<tr><td colspan="2">' + e.name +
+        ':</td><td colspan="2">' + e.value + '</td></tr>';
+    });
+  }
+  return ret;
+}
+
+function tipFooter(creature) {
+  return '<tr><td colspan="4" height="5"></td></tr><tr>' +
+  '<td class="header" colspan="4" class="fshCenter">Description</td>' +
+  '</tr><tr><td colspan="4">' + creature.description + '</td></tr>' +
+  '<tr><td colspan="4" height="5"></td></tr></table></td></tr>' +
+  '<tr><td class="fshCenter"><b>' + creature.name + '</b></td></tr>' +
+  '</table>';
+}
+
+function makeMonsterTip(creature, oneHitNumber, myLvlClas) {
+  return tipHeader(creature) +
+    tipClassLevel(creature, myLvlClas) +
+    tipAttackDefense(creature) +
+    tipArmorDamage(creature) +
+    tipHp(creature, oneHitNumber) +
+    tipSpacer +
+    tipEnhancements(creature) +
+    tipFooter(creature);
+}
+
 function doMouseOver(creature, monster) {
   var oneHitNumber = Math.ceil(creature.hp * calf.hpVariable + creature.armor *
     calf.generalVariable);
   var myLvlClas = 'fshYellow';
   if (statLevel > creature.level) {myLvlClas = 'fshRed';}
-  var monsterTip = '<table><tr><td>' +
-    '<img src="https://cdn.fallensword.com/creatures/' + creature.image_id +
-    '.jpg" height="200" width="200"></td><td rowspan="2">' +
-    '<table width="400"><tr>' +
-    '<td class="header" colspan="4" class="fshCenter">Statistics</td></tr>' +
-    '<tr><td>Class:&nbsp;</td><td width="40%">' + creature.creature_class +
-    '</td><td>Level:&nbsp;</td><td width="40%">' + creature.level +
-    ' (your level:<span class="' + myLvlClas + '">' +
-    statLevel + '</span>)</td>' +
-    '</tr><tr><td>Attack:&nbsp;</td><td width="40%">' + creature.attack +
-    ' (your defense:<span class="fshYellow">' + statDefense + '</span>)</td>' +
-    '<td>Defense:&nbsp;</td><td width="40%">' + creature.defense +
-    ' (your attack:<span class="fshYellow">' + statAttack + '</span>)</td>' +
-    '</tr><tr><td>Armor:&nbsp;</td><td width="40%">' + creature.armor +
-    ' (your damage:<span class="fshYellow">' + statDamage + '</span>)</td>' +
-    '<td>Damage:&nbsp;</td><td width="40%">' + creature.damage +
-    ' (your armor:<span class="fshYellow">' + statArmor + '</span>)</td>' +
-    '</tr><tr><td>HP:&nbsp;</td><td width="40%">' + creature.hp +
-    ' (your HP:<span class="fshYellow">' + statHp + '</span>)' +
-    '(1H: <span class="fshRed">' + oneHitNumber + '</span>)</td>' +
-    '<td>Gold:&nbsp;</td><td width="40%">' + creature.gold + '</td></tr>' +
-    '<tr><td colspan="4" height="5"></td></tr><tr>' +
-    '<td class="header" colspan="4" class="fshCenter">Enhancements</td></tr>';
-
-  if (!creature.enhancements) {
-    monsterTip += '<tr><td colspan="4">[no enhancements]</td></tr>';
-  } else {
-    creature.enhancements.forEach(function(e) {
-      monsterTip += '<tr><td colspan="2">' + e.name +
-        ':</td><td colspan="2">' + e.value + '</td></tr>';
-    });
-  }
-
-  monsterTip += '<tr><td colspan="4" height="5"></td></tr><tr>' +
-    '<td class="header" colspan="4" class="fshCenter">Description</td>' +
-    '</tr><tr><td colspan="4">' + creature.description + '</td></tr>' +
-    '<tr><td colspan="4" height="5"></td></tr></table></td></tr>' +
-    '<tr><td class="fshCenter"><b>' + creature.name + '</b></td></tr>' +
-    '</table>';
-
-  monster.dataset.tipped = monsterTip;
+  monster.dataset.tipped = makeMonsterTip(creature, oneHitNumber, myLvlClas);
 }
 
 var bailOut = [
