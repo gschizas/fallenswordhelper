6c396bb58695f6da434fabde09a70d65351ea7d6
atompkins <atompkins@gmail.com>
2018-10-12 23:16:11 +0100

    refactor logPrefs

--- src/modules/settings/logPrefs.js
+++ src/modules/settings/logPrefs.js
@@ -2,48 +2,53 @@ import getValue from '../system/getValue';
 import isChecked from '../system/isChecked';
 import {helpLink, simpleCheckbox} from './simpleCheckbox';
 
+function newGuildLogHistory() {
+  return '<tr><td class="fshRight">New Guild Log History' +
+    helpLink('New Guild Log History (pages)',
+      'This is the number of pages that the new guild log ' +
+      'screen will go back in history.') +
+    ':</td><td><input name="newGuildLogHistoryPages" size="3" value="' +
+    getValue('newGuildLogHistoryPages') + '"></td></td></tr>';
+}
+
+function newLogMessageSound() {
+  return '<tr><td class="fshRight">New Log Message Sound' +
+    helpLink('New Log Message Sound',
+      'The .wav or .ogg file to play when you have unread log messages. ' +
+      'This must be a .wav or .ogg file. This option can be turned on/off ' +
+      'on the world page. Only works in Firefox 3.5+') +
+    ':</td><td colspan="3"><input name="defaultMessageSound" size="60" ' +
+    'value="' + getValue('defaultMessageSound') +
+    '"></td></tr>';
+}
+
+function playSoundOnUnreadLog() {
+  return '<tr><td class="fshRight">Play sound on unread log' +
+    helpLink('Play sound on unread log',
+      'Should the above sound play when you have unread log messages? ' +
+      '(will work on Firefox 3.5+ only)') +
+    ':</td><td><input name="playNewMessageSound" type="checkbox" ' +
+    'value="on"' +
+    isChecked(getValue('playNewMessageSound')) + '>' +
+    ' Show speaker on world' +
+    helpLink('Show speaker on world',
+      'Should the toggle play sound speaker show on the world map? ' +
+      '(This icon is next to the Fallensword wiki icon and will only ' +
+      'display on Firefox 3.5+)') +
+    ':<input name="showSpeakerOnWorld" type="checkbox" value="on"' +
+    isChecked(getValue('showSpeakerOnWorld')) +
+    '></tr></td>';
+}
+
 export default function logPrefs() {
   // Log screen prefs
-  return '<tr><th colspan="2"><b>Log screen preferences' +
-      '</b></th></tr>' +
-
+  return '<tr><th colspan="2"><b>Log screen preferences</b></th></tr>' +
     simpleCheckbox('hideNonPlayerGuildLogMessages') +
     simpleCheckbox('useNewGuildLog') +
-
-    '<tr><td class="fshRight">New Guild Log History' +
-      helpLink('New Guild Log History (pages)',
-        'This is the number of pages that the new guild log ' +
-        'screen will go back in history.') +
-      ':</td><td><input name="newGuildLogHistoryPages" size="3" value="' +
-      getValue('newGuildLogHistoryPages') + '"></td></td></tr>' +
-
+    newGuildLogHistory() +
     simpleCheckbox('enableLogColoring') +
-
-    '<tr><td class="fshRight">New Log Message Sound' +
-      helpLink('New Log Message Sound',
-        'The .wav or .ogg file to play when you have unread log messages. ' +
-        'This must be a .wav or .ogg file. This option can be turned on/off ' +
-        'on the world page. Only works in Firefox 3.5+') +
-      ':</td><td colspan="3"><input name="defaultMessageSound" size="60" ' +
-      'value="' + getValue('defaultMessageSound') +
-      '"></td></tr>' +
-
-    '<tr><td class="fshRight">Play sound on unread log' +
-      helpLink('Play sound on unread log',
-        'Should the above sound play when you have unread log messages? ' +
-        '(will work on Firefox 3.5+ only)') +
-      ':</td><td><input name="playNewMessageSound" type="checkbox" ' +
-      'value="on"' +
-      isChecked(getValue('playNewMessageSound')) + '>' +
-      ' Show speaker on world' +
-      helpLink('Show speaker on world',
-        'Should the toggle play sound speaker show on the world map? ' +
-        '(This icon is next to the Fallensword wiki icon and will only ' +
-        'display on Firefox 3.5+)') +
-      ':<input name="showSpeakerOnWorld" type="checkbox" value="on"' +
-      isChecked(getValue('showSpeakerOnWorld')) +
-      '></tr></td>' +
-
+    newLogMessageSound() +
+    playSoundOnUnreadLog() +
     simpleCheckbox('enableChatParsing') +
     simpleCheckbox('keepBuffLog') +
     simpleCheckbox('addAttackLinkToLog') +
