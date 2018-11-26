6ff3170e7dd43177ee7e0096ef5ac1aaaa9334b8
atompkins <atompkins@gmail.com>
2018-11-26 09:54:09 +0000

    Build 15.23.52

--- dist/dev/calfSystem.js
+++ dist/dev/calfSystem.js
@@ -4298,7 +4298,10 @@
 
   function playerName() {
     if (!thisPlayerName) {
-      thisPlayerName = getElementById('statbar-character').textContent;
+      var statBarCharacter = getElementById('statbar-character');
+      if (statBarCharacter) {
+        thisPlayerName = statBarCharacter.textContent;
+      }
     }
     return thisPlayerName;
   }
@@ -15399,7 +15402,10 @@
   }
 
   function getBuff$1(name) {
-    return GameData.player().buffs.find(function(e) {return e.name === name;});
+    var buffs = GameData.player().buffs;
+    if (buffs) {
+      return buffs.find(function(e) {return e.name === name;});
+    }
   }
 
   function getCooldown() {
@@ -20089,7 +20095,7 @@
   }
 
   window.FSH = window.FSH || {};
-  window.FSH.calf = '51';
+  window.FSH.calf = '52';
 
   // main event dispatcher
   window.FSH.dispatch = function dispatch() {
