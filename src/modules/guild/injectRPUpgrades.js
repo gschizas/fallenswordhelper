f618742f306a28c63916a09f8a0ec97384475f93
atompkins <atompkins@gmail.com>
2018-12-20 13:49:12 +0000

    refactor and fix RegExp

--- src/modules/guild/injectRPUpgrades.js
+++ src/modules/guild/injectRPUpgrades.js
@@ -2,25 +2,28 @@ import insertHtmlBeforeEnd from '../common/insertHtmlBeforeEnd';
 import jQueryNotPresent from '../common/jQueryNotPresent';
 import myStats from '../ajax/myStats';
 import {pCC} from '../support/layout';
+import partial from '../common/partial';
 import reduceBuffArray from '../common/reduceBuffArray';
 
-var packRE = />([ a-zA-Z]+) Level (\d+)/g;
+var packRE = />\s*([ a-zA-Z]+) Level (\d+)/g;
+
+function checkForBuffs(myBuffs, el) {
+  var tipped = el.dataset.tipped;
+  var packBuffs;
+  while ((packBuffs = packRE.exec(tipped)) !== null) {
+    if (myBuffs[packBuffs[1]] === Number(packBuffs[2])) {
+      insertHtmlBeforeEnd(el.parentNode,
+        '<br><span class="fshRed fshNoWrap">' + packBuffs[1] + ' ' +
+        packBuffs[2] + ' active</span>');
+    }
+  }
+}
 
 function postWarnings(myBuffs) {
   var packsRow = pCC.firstElementChild.rows[9];
   if (!packsRow) {return;}
   var nodeList = packsRow.cells[0].firstElementChild.getElementsByTagName('A');
-  Array.prototype.forEach.call(nodeList, function(el) {
-    var tipped = el.dataset.tipped;
-    var packBuffs;
-    while ((packBuffs = packRE.exec(tipped)) !== null) {
-      if (myBuffs[packBuffs[1]] === Number(packBuffs[2])) {
-        insertHtmlBeforeEnd(el.parentNode,
-          '<br><span class="fshRed fshNoWrap">' + packBuffs[1] + ' ' +
-          packBuffs[2] + ' active</span>');
-      }
-    }
-  });
+  Array.prototype.forEach.call(nodeList, partial(checkForBuffs, myBuffs));
 }
 
 function parseProfile(data) {
