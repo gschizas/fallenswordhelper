f13435aae5e34c6e9443ac89c26512f5fe2c0f60
atompkins <atompkins@gmail.com>
2019-01-01 10:44:18 +0000

    refactor

--- src/modules/common/formatLocalDateTime.js
+++ src/modules/common/formatLocalDateTime.js
@@ -1,37 +1,20 @@
+import formatDateTime from './formatDateTime';
 import isDate from './isDate';
 import padZ from '../system/padZ';
 
-function yearAsString(aDate) {
-  return aDate.getFullYear().toString();
-}
-
-function paddedMonthNumber(aDate) {
-  return padZ(aDate.getMonth() + 1);
-}
-
-function paddedDayNumber(aDate) {
-  return padZ(aDate.getDate());
-}
-
-function padddedHours(aDate) {
-  return padZ(aDate.getHours());
-}
-
-function paddedMinutes(aDate) {
-  return padZ(aDate.getMinutes());
-}
-
-function paddedSeconds(aDate) {
-  return padZ(aDate.getSeconds());
+function localDateParts(aDate) {
+  return [
+    aDate.getFullYear().toString(),
+    padZ(aDate.getMonth() + 1),
+    padZ(aDate.getDate()),
+    padZ(aDate.getHours()),
+    padZ(aDate.getMinutes()),
+    padZ(aDate.getSeconds())
+  ];
 }
 
 export default function formatLocalDateTime(aDate) {
-  if (isDate) {
-    return yearAsString(aDate) + '-' +
-      paddedMonthNumber(aDate) + '-' +
-      paddedDayNumber(aDate) + ' ' +
-      padddedHours(aDate) + ':' +
-      paddedMinutes(aDate) + ':' +
-      paddedSeconds(aDate);
+  if (isDate(aDate)) {
+    return formatDateTime(localDateParts(aDate));
   }
 }
