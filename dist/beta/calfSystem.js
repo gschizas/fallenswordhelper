be442916aa8df20db5584ef78254b324d575d7b2
atompkins <atompkins@gmail.com>
2019-01-18 11:23:10 +0000

    Build 15.23.85

--- dist/beta/calfSystem.js
+++ dist/beta/calfSystem.js
@@ -20837,17 +20837,23 @@
     doCheck$1(true, checkbox);
   }
 
+  function doTheChecks(itemid, itemTables) {
+    itemTables
+      .filter(partial(notInSt, findStCheck()))
+      .map(getCheckbox)
+      .filter(partial(thisType, itemid))
+      .slice(0, getHowMany(itemTables))
+      .forEach(checkAll$1);
+  }
+
   function doCheckAll$1(evt) {
     var itemList = getElementById('item-div') ||
       getElementById('item-list');
     var itemTables = querySelectorArray('table:not(.fshHide)', itemList);
-    itemTables.map(getCheckbox).forEach(unCheckAll);
     itemTables
-      .filter(partial(notInSt, findStCheck()))
       .map(getCheckbox)
-      .filter(partial(thisType, evt.target.id))
-      .slice(0, getHowMany(itemTables))
-      .forEach(checkAll$1);
+      .forEach(unCheckAll);
+    doTheChecks(evt.target.id, itemTables);
   }
 
   function toggleAllPlants(evt) {
@@ -20915,7 +20921,7 @@
     trade: trade,
     titan: {'-': {'-': injectTitan}},
     toprated: toprated,
-    inventing: {viewrecipe: {'-': {'-': inventing}}},
+    inventing: {viewrecipe: {'-': inventing}},
     tempinv: {'-': {'-': injectMailbox}},
     findplayer: {'-': {'-': injectFindPlayer}},
     quests: quests, // UFSG
@@ -21003,7 +21009,7 @@
   }
 
   window.FSH = window.FSH || {};
-  window.FSH.calf = '84';
+  window.FSH.calf = '85';
 
   // main event dispatcher
   window.FSH.dispatch = function dispatch() {
