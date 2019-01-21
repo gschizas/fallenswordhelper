1f6fa63d00dddfbcde762dea5d9f33768342565d
atompkins <atompkins@gmail.com>
2019-01-21 12:50:53 +0000

    handle messages in trade screen

--- src/modules/chrome/pageSwitcher/trade.js
+++ src/modules/chrome/pageSwitcher/trade.js
@@ -2,6 +2,7 @@ import injectTrade from '../../trade/trade';
 
 export default {
   '-': {'-': injectTrade},
+  sendgold: {'-': injectTrade},
   createsecure: {'-': injectTrade},
   docreatesecure: {'-': injectTrade}
 };
