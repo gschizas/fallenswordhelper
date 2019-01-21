b6222bf2761e0748656a0f6883600268584524e4
atompkins <atompkins@gmail.com>
2019-01-21 12:52:17 +0000

    improve performance for guild report

--- src/modules/common/classHandler.js
+++ src/modules/common/classHandler.js
@@ -1,9 +1,7 @@
+import classPair from './classPair';
 import handleEvent from './handleEvent';
-import hasClass from './hasClass';
 import partial from './partial';
 
-function passingTest(self, el) {return hasClass(el[0], self);}
-
 export default function classHandler(evtAry) {
-  return partial(handleEvent, passingTest, evtAry);
+  return partial(handleEvent, classPair, evtAry);
 }
--- src/modules/guild/guildReport/eventHandlers.js
+++ src/modules/guild/guildReport/eventHandlers.js
@@ -1,64 +1,96 @@
 import classHandler from '../../common/classHandler';
+import classPair from '../../common/classPair';
 import equipItem from '../../ajax/equipItem';
 import getElementsByTagName from '../../common/getElementsByTagName';
-import hideQTip from '../../common/hideQTip';
 import {imageServer} from '../../system/system';
 import on from '../../common/on';
 import {pCC} from '../../support/layout';
 import partial from '../../common/partial';
+import playerId from '../../common/playerId';
 import {queueRecallItem} from '../../ajaxQueue/queue';
+import {sendEvent} from '../../support/fshGa';
 
 var spinner = '<span class="guildReportSpinner" style="background-image: ' +
   'url(\'' + imageServer + '/skin/loading.gif\');"></span>';
 
-function recalled(theTd, data) {
-  if (data.r === 1) {return;}
+function recalled(theTd) {
   theTd.innerHTML = '<span class="fastWorn">' +
     'You successfully recalled the item</span>';
 }
 
-function recallInfObj(self, mode, href) {
-  return {
-    invId: href.match(/&id=(\d+)/)[1],
-    playerId: href.match(/&player_id=(\d+)/)[1],
-    mode: mode,
-    action: self.getAttribute('action')
-  };
-}
-
-function recallItem(self) { // jQuery
-  hideQTip(self);
-  var mode = self.getAttribute('mode');
-  var theTd = self.parentNode.parentNode;
-  if (mode === '0') {theTd = theTd.parentNode;}
-  var href = theTd.children[0].href;
-  if (!href) {return;}
-  queueRecallItem(recallInfObj(self, mode, href))
-    .done(partial(recalled, theTd));
-  theTd.innerHTML = spinner;
-}
-
-function wornItem(theTd, data) {
-  if (data.r === 1) {return;}
+function wornItem(theTd) {
   theTd.innerHTML = '<span class="fastWorn">Worn</span>';
-}
-
-function wearItem(self) { // jQuery
-  hideQTip(self);
-  var theTd = self.parentNode.parentNode.parentNode;
-  var href = theTd.children[0].href;
-  if (!href) {return;}
-  equipItem(href.match(/&id=(\d+)/)[1]).done(partial(wornItem, theTd));
-  theTd.innerHTML = spinner;
 }
 
 function replyTo(self) {
   window.openQuickMsgDialog(self.getAttribute('target_player'));
 }
 
+function itemId(href) {
+  return href.match(/&id=(\d+)/)[1];
+}
+
+function targetPlayerId(href) {
+  return href.match(/&player_id=(\d+)/)[1];
+}
+
+function recallResult(action, theTd, data) {
+  if (data.r === 1) {return;}
+  if (action === 'recall') {
+    recalled(theTd);
+  } else {
+    wornItem(theTd);
+  }
+}
+
+function doRecall(theTd, href, mode, action) {
+  queueRecallItem({
+    invId: itemId(href),
+    playerId: targetPlayerId(href),
+    mode: mode,
+    action: action
+  }).done(partial(recallResult, action, theTd));
+}
+
+function recallTo(theTd, href, mode) {
+  doRecall(theTd, href, mode, 'recall');
+}
+
+function doFastBp(theTd, href) {
+  sendEvent('GuildReport', 'Fast BP');
+  recallTo(theTd, href, 0);
+}
+
+function doFastGs(theTd, href) {
+  sendEvent('GuildReport', 'Fast GS');
+  recallTo(theTd, href, 1);
+}
+
+function doFastWear(theTd, href) {
+  sendEvent('GuildReport', 'Fast Wear');
+  if (Number(targetPlayerId(href)) === playerId()) {
+    equipItem(itemId(href)).done(partial(wornItem, theTd));
+  } else {
+    doRecall(theTd, href, 0, 'wear');
+  }
+}
+
+var subClass = [
+  ['fast-bp', doFastBp],
+  ['fast-gs', doFastGs],
+  ['fast-wear', doFastWear]
+];
+
+function doFastRecall(self) {
+  var theTd = self.parentNode.parentNode;
+  var href = theTd.children[0].href;
+  if (!href) {return;}
+  subClass.find(partial(classPair, self))[1](theTd, href);
+  theTd.innerHTML = spinner;
+}
+
 var classEvts = [
-  ['recall', recallItem],
-  ['equip', wearItem],
+  ['sendLink', doFastRecall],
   ['a-reply', replyTo]
 ];
 
