08f7ef41f72335f58991f2765a45f5af8dd7c624
atompkins <atompkins@gmail.com>
2019-01-29 06:23:49 +0000

    tidy

--- src/modules/scavenging/lookForMultiplierCount.js
+++ src/modules/scavenging/lookForMultiplierCount.js
@@ -1,16 +1,13 @@
+import {closestTable} from '../common/closest';
 import {createSpan} from '../common/cElement';
 import {getElementById} from '../common/getElement';
 import getText from '../common/getText';
 import insertElement from '../common/insertElement';
 import intValue from '../system/intValue';
 import on from '../common/on';
+import partial from '../common/partial';
 
-var multCnt;
-var maxTimes;
-var statbarGold;
-var gold;
-
-function setMaxTimes() {
+function setMaxTimes(maxTimes, statbarGold, gold) {
   if (maxTimes) {
     maxTimes.innerHTML = '';
     var scoutGold = Number(gold.value);
@@ -22,16 +19,19 @@ function setMaxTimes() {
   }
 }
 
-function foundMultiplierCount() {
-  maxTimes = createSpan();
+function foundMultiplierCount(multCnt) {
+  var parentTable = closestTable(multCnt);
+  parentTable.removeAttribute('width');
+  var maxTimes = createSpan();
   insertElement(multCnt.parentNode, maxTimes);
-  statbarGold = getElementById('statbar-gold');
-  gold = getElementById('gold');
-  setMaxTimes();
-  on(gold, 'keyup', setMaxTimes);
+  var statbarGold = getElementById('statbar-gold');
+  var gold = getElementById('gold');
+  var boundSet = partial(setMaxTimes, maxTimes, statbarGold, gold);
+  boundSet();
+  on(gold, 'keyup', boundSet);
 }
 
 export default function lookForMultiplierCount() {
-  multCnt = getElementById('multiplier_count');
-  if (multCnt) {foundMultiplierCount();}
+  var multCnt = getElementById('multiplier_count');
+  if (multCnt) {foundMultiplierCount(multCnt);}
 }
