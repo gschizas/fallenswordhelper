4eeaea3e8fb224bc2e0097ddea8823126b9add1b
atompkins <atompkins@gmail.com>
2019-03-13 10:10:56 +0000

    active bounty target format change

--- src/modules/chrome/activeWantedBounties/basicBounty.js
+++ src/modules/chrome/activeWantedBounties/basicBounty.js
@@ -1,16 +1,21 @@
 import getText from '../../common/getText';
 import querySelector from '../../common/querySelector';
 
+function getTarget(firstCell) {
+  if (firstCell.children[0].tagName === 'A') {return firstCell.children[0];}
+  return firstCell.children[0].children[0];
+}
+
 function rewardType(theCells) {
   return querySelector('img', theCells[2]).title;
 }
 
 export default function basicBounty(theCells) {
-  var targetData = theCells[0].children[0].children[0];
+  var targetData = getTarget(theCells[0]);
   return {
     target: getText(targetData),
     link: targetData.href,
-    lvl: getText(targetData.nextSibling).replace(/[[|\]]/, ''), // Text Node
+    lvl: getText(targetData.nextSibling).replace(/[[|\]]/g, ''), // Text Node
     reward: getText(theCells[2]),
     rewardType: rewardType(theCells),
     posted: getText(theCells[3]),
