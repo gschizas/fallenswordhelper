89a3d75223b5b17899f95ead6ed7545e9e3d7967
atompkins <atompkins@gmail.com>
2019-03-13 10:11:21 +0000

    unhandledrejection handler for chrome

--- src/modules/support/globalErrorHandler.js
+++ src/modules/support/globalErrorHandler.js
@@ -4,18 +4,27 @@ import {sendException} from './fshGa';
 
 var enabled;
 
-function logError(e) {
-  if (e.error) {
-    var msg = parseError(e.error);
+function handleMsgStack(stuff) {
+  if (stuff) {
+    var msg = parseError(stuff);
     if (msg.includes('calfSystem')) {
       sendException(msg, true);
     }
   }
 }
 
+function logError(e) {
+  handleMsgStack(e.error);
+}
+
+function unhandledrejection(e) {
+  handleMsgStack(e.reason);
+}
+
 export default function globalErrorHandler() {
   if (!enabled) {
     on(window, 'error', logError);
+    on(window, 'unhandledrejection', unhandledrejection);
     enabled = true;
   }
 }
