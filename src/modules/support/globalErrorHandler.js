d1772ccd1a5a75ceb9d19be4bc146e1c77649cdf
atompkins <atompkins@gmail.com>
2019-03-16 22:13:19 +0000

    custom ajax error

--- src/modules/support/globalErrorHandler.js
+++ src/modules/support/globalErrorHandler.js
@@ -8,21 +8,24 @@ function handleMsgStack(type, stuff) {
   var msg = parseError(stuff);
   if (msg.includes('calfSystem')) {
     sendException(type + msg, true);
+    return true;
   }
 }
 
 function handleError(type, stuff) {
   if (stuff) {
-    handleMsgStack(type, stuff);
+    return handleMsgStack(type, stuff);
   }
 }
 
 function logError(e) {
-  handleError('window onerror', e.error);
+  handleError('window onerror ', e.error);
 }
 
 function unhandledrejection(e) {
-  handleError('Uncaught (in promise) ', e.reason);
+  if (handleError('Uncaught (in promise) ', e.reason)) {
+    e.preventDefault();
+  }
 }
 
 export default function globalErrorHandler() {
