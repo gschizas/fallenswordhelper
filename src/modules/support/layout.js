9920167faab2ccbf41e928c976ad2fe22283f683
atompkins <atompkins@gmail.com>
2019-03-19 07:13:06 +0000

    message sound native

--- src/modules/support/layout.js
+++ src/modules/support/layout.js
@@ -1,10 +1,12 @@
 import {getElementById} from '../common/getElement';
 
+export var pCL;
 export var pCC;
 export var pCR;
 
 export function initPcc() {
   if (!pCC) {
+    pCL = getElementById('pCL');
     pCC = getElementById('pCC');
     pCR = getElementById('pCR');
   }
