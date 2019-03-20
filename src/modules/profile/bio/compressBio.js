102926e7b5c4400025ac211befa564e13a308131
atompkins <atompkins@gmail.com>
2019-03-20 22:23:04 +0000

    bio toggle telemetry

--- src/modules/profile/bio/compressBio.js
+++ src/modules/profile/bio/compressBio.js
@@ -1,10 +1,17 @@
 import insertElement from '../../common/insertElement';
+import on from '../../common/on';
+import {sendEvent} from '../../support/fshGa';
 import {createDiv, createInput, createLabel} from '../../common/cElement';
+
+function bioToggle() {
+  sendEvent('bio', 'toggle');
+}
 
 function doCompression(bioCell) {
   var fshCompressor = createDiv({className: 'fshCompressor'});
-  insertElement(fshCompressor,
+  var toggle = insertElement(fshCompressor,
     createInput({id: 'fshCompressToggle', type: 'checkbox'}));
+  on(toggle, 'change', bioToggle);
   insertElement(fshCompressor,
     createLabel({className: 'sendLink', htmlFor: 'fshCompressToggle'}));
   var fshCompress = insertElement(fshCompressor,
