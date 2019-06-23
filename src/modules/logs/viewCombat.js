715467f6fb52c5a057e1026cc52bb5ce1fbd8470
atompkins <atompkins@gmail.com>
2019-06-23 21:10:38 +0100

    log view arena combat DEV

---
+++ src/modules/logs/viewCombat.js
@@ -0,0 +1,20 @@
+// import results from '../app/arena/results';
+
+function getPvpId(aRow) {
+  return aRow.cells[2] && /pvp_id=(\d+)/.exec(aRow.cells[2].innerHTML);
+}
+
+// function processResults(json) {
+//   console.log(json);
+// }
+
+function havePvpId(aRow, pvpId) {
+  // results(pvpId).then(processResults);
+  console.log(aRow, pvpId); // eslint-disable-line no-console
+}
+
+// TODO Pref
+export default function viewCombat(aRow) {
+  const pvpId = getPvpId(aRow);
+  if (pvpId) {havePvpId(aRow, pvpId[1]);}
+}
