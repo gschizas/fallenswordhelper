00adf2a8931b91e6d591d7ad0580c6d8bcf36288
atompkins <atompkins@gmail.com>
2019-06-25 23:47:04 +0100

    oops - used wrong storeitems for inventory

--- src/modules/ajax/storeItems.js
+++ src/modules/ajax/storeItems.js
@@ -1,6 +1,5 @@
 import ajaxReturnCode from '../app/ajaxReturnCode';
 import daStoreItems from '../_dataAccess/daStoreItems';
-// import dostoreitems from '../app/guild/inventory/dostoreitems';
 import errorDialog from '../app/errorDialog';
 
 export default function storeItems(invIdAry) {
