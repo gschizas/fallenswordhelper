9f7ce4ee8a3a96ce843c07a384811eb9367164b8
atompkins <atompkins@gmail.com>
2019-06-27 09:36:33 +0100

    SE Log fallback

---
+++ src/modules/_dataAccess/superelite.js
@@ -0,0 +1,36 @@
+import {arrayFrom} from '../common/arrayFrom';
+import createDocument from '../system/createDocument';
+import {dataRows} from '../common/dataRows';
+import getTextTrim from '../common/getTextTrim';
+import indexAjaxData from '../ajax/indexAjaxData';
+import {months} from '../support/constants';
+import querySelector from '../common/querySelector';
+import {now, nowSecs} from '../support/now';
+
+function parseDateAsOffset(textDate) {
+  var dateAry = textDate.replace('<br>', ' ').split(/[: /]/);
+  return Math.floor(
+    (now - Date.UTC(Number(dateAry[2]), months.indexOf(dateAry[1]),
+      Number(dateAry[0]), Number(dateAry[3]), Number(dateAry[4]), 0)) / 1000
+  );
+}
+
+function formatRow(row) {
+  return {
+    time: parseDateAsOffset(row.cells[0].innerHTML),
+    creature: {name: getTextTrim(row.cells[1])}
+  };
+}
+
+function parseReport(html) {
+  const doc = createDocument(html);
+  const logTable = querySelector('#pCC table table', doc);
+  const rows = arrayFrom(logTable.rows).filter(dataRows(4, 1));
+  const data = rows.map(formatRow);
+  return {r: data, s: true, t: '0 ' + String(nowSecs)};
+}
+
+// Incomplete
+export default function superelite() {
+  return indexAjaxData({cmd: 'superelite'}).then(parseReport);
+}
