921357e355cb4fc9cc90285761b91530c13820c0
atompkins <atompkins@gmail.com>
2019-07-02 18:35:32 +0100

    rank weighting fallback

--- src/modules/guild/ranks/weightings.js
+++ src/modules/guild/ranks/weightings.js
@@ -1,15 +1,15 @@
 import bitwiseAnd from '../../common/bitwiseAnd';
-import {createInput} from '../../common/cElement';
+import daRanksView from '../../_dataAccess/daRanksView';
 import getText from '../../common/getText';
-import hideElement from '../../common/hideElement';
 import insertElement from '../../common/insertElement';
 import insertHtmlAfterBegin from '../../common/insertHtmlAfterBegin';
 import insertHtmlBeforeEnd from '../../common/insertHtmlBeforeEnd';
 import on from '../../common/on';
 import partial from '../../common/partial';
 import querySelector from '../../common/querySelector';
-import ranks from '../../app/guild/ranks/ranks';
+import replaceChild from '../../common/replaceChild';
 import roundToString from '../../common/roundToString';
+import {createInput, createSpan} from '../../common/cElement';
 
 var privLookup = [
   [0x2, 5], //       Bank Withdraw
@@ -24,25 +24,20 @@ var privLookup = [
   [0x400000, 4] //   Can Kick Members
 ];
 
-function rankObj(rankName, rank) {return rank.name === rankName;}
-
-function sum(prev, curr) {return prev + curr;}
-
-function thePriv(perms, v) {return bitwiseAnd(perms, v[0]);}
-
-function addWeight(prev, curr) {return prev + curr[1] - 1;}
-
 function calcPermWeight(perms) {
   return roundToString(
-    privLookup.filter(partial(thePriv, perms)).reduce(addWeight, 0) +
-    perms.toString(2).split('').map(Number).reduce(sum, 0), 1);
+    privLookup.filter(([flag]) => bitwiseAnd(perms, flag))
+      .reduce((a, [, weight]) => a + weight - 1, 0) +
+    perms.toString(2).split('').map(Number).reduce((a, b) => a + b, 0)
+    , 1
+  );
 }
 
 function parseRankData(memberRanks, row) {
   // Makes a weighted calculation of available permissions and gets tax rate
   var rankCell = row.children[0];
   var rankName = getText(rankCell.firstChild); // Text Node
-  var thisRank = memberRanks.find(partial(rankObj, rankName));
+  var thisRank = memberRanks.find(r => r.name === rankName);
   if (thisRank) {
     insertHtmlAfterBegin(rankCell, '<span class="fshBlue">(' +
       calcPermWeight(thisRank.permissions) +
@@ -50,15 +45,21 @@ function parseRankData(memberRanks, row) {
   }
 }
 
-function gotRankData(theRows, json) {
+function gotRankData(theRows, spinner, json) {
   if (json.s) {
     theRows.forEach(partial(parseRankData, [json.r['0']].concat(json.r.ranks)));
+    spinner.classList.remove('fshSpinner');
   }
 }
 
-function fetchRankData(theRows, weightButton) { // jQuery.min
-  hideElement(weightButton);
-  ranks().then(partial(gotRankData, theRows));
+function fetchRankData(theRows, weightButton) {
+  const spinner =
+    createSpan({
+      className: 'fshBlock fshRelative fshSpinner fshSpinner12',
+      style: {height: '15px', width: '136px'}
+    });
+  replaceChild(spinner, weightButton);
+  daRanksView().then(partial(gotRankData, theRows, spinner));
 }
 
 function injectWeightButton(theRows, addNewRank) {
