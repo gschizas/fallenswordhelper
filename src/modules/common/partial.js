3772e0d23050edfcc13e7416691a3bed0db03060
atompkins <atompkins@gmail.com>
2019-07-03 11:35:04 +0100

    cleaner partial application

--- src/modules/common/partial.js
+++ src/modules/common/partial.js
@@ -1,5 +1,3 @@
-import {arrayFrom} from './arrayFrom';
-
-export default function partial(fn /* , rest args */) {
-  return fn.bind.apply(fn, arrayFrom(arguments));
+export default function partial(fn, ...args) {
+  return fn.bind(fn, ...args);
 }
