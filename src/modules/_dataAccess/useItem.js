ff396c9735c28290d91c99c40a9eaae52400c5b2
atompkins <atompkins@gmail.com>
2019-07-17 09:28:22 +0100

    guards

--- src/modules/_dataAccess/useItem.js
+++ src/modules/_dataAccess/useItem.js
@@ -1,6 +1,7 @@
 import {composingFragmentType} from '../support/constants';
 import indexAjaxData from '../ajax/indexAjaxData';
 import infoBoxFrom from '../common/InfoBoxFrom';
+import {sendEvent} from '../support/fshGa';
 
 const extract = info => ({r: {item: {n: info.match(/'(.*)'/)[1]}}, s: true});
 
@@ -13,9 +14,15 @@ function fragObj(pair) {
 }
 
 function stash(info) {
-  const frags = info.match(/You gained (.*) Fragments/)[1].split(', ')
-    .map(fragObj);
-  return {r: {frags}, s: true};
+  const reAry = info.match(/You gained (.*) Fragments/);
+  if (reAry) {
+    const frags = reAry[1].split(', ').map(fragObj);
+    return {r: {frags}, s: true};
+  }
+  sendEvent('da/useItem', 'Bad Msg', info);
+  //#if _DEV  //  da/useItem Bad Msg
+  console.log('da/useItem', 'Bad Msg', info); // eslint-disable-line no-console
+  //#endif
 }
 
 const outputLookup = [
@@ -27,8 +34,16 @@ const outputLookup = [
 
 function formatResults(html) {
   const info = infoBoxFrom(html);
-  const thisResult = outputLookup.find(e => info.startsWith(e[0]));
-  if (thisResult) {return thisResult[1](info);}
+  if (info) {
+    const thisResult = outputLookup.find(e => info.startsWith(e[0]));
+    if (thisResult) {return thisResult[1](info);}
+  } else {
+    sendEvent('da/useItem', 'No Info');
+    //#if _DEV  //  da/useItem No Info
+    console.log('da/useItem', 'No Info'); // eslint-disable-line no-console
+    //#endif
+    return {s: false};
+  }
   return {e: {message: info}, s: false};
 }
 
