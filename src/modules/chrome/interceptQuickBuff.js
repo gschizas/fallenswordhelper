b7b930c017047426770d69f439723a87c3984c34
atompkins <atompkins@gmail.com>
2019-08-05 15:43:51 +0100

    Fix for guild chat buff links

--- src/modules/chrome/interceptQuickBuff.js
+++ src/modules/chrome/interceptQuickBuff.js
@@ -1,15 +1,4 @@
-function getPos(available, desired, offset) {
-  return Math.floor(Math.max(available - desired, 0) / 2 + offset);
-}
-
-function fshOpen(url, title, w, _h, features) {
-  var h = _h;
-  if (_h === 500) {h = 1000;}
-  var top = getPos(window.screen.availHeight, h, window.screenY);
-  var left = getPos(document.documentElement.clientWidth, w, window.screenX);
-  window.open(url, title, 'width=' + w + ', height=' + h + ', left=' + left +
-    ', top=' + top + features);
-}
+import fshOpen from './fshOpen';
 
 export default function interceptQuickBuff() {
   window.openWindow = fshOpen;
