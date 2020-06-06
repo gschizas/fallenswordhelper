f0033b885bb86892aebcdd83d06506c734bca7f0
atompkins <atompkins@gmail.com>
2020-06-06 20:12:05 +0100

    fix for guild history compress

--- src/modules/guild/compressHistory.js
+++ src/modules/guild/compressHistory.js
@@ -1,10 +1,19 @@
 import compressBio from '../profile/bio/compressBio';
+import createDiv from '../common/cElement/createDiv';
 import { defTable } from '../support/constants';
 import getArrayByTagName from '../common/getArrayByTagName';
+import insertElement from '../common/insertElement';
 import { pCC } from '../support/layout';
+import setInnerHtml from '../dom/setInnerHtml';
 
 export default function compressHistory() {
-  compressBio(
-    getArrayByTagName(defTable, pCC).slice(-2, -1)[0].rows[0].cells[0],
-  );
+  const thisCell = getArrayByTagName(defTable, pCC).slice(-2, -1)[0]
+    .rows[0].cells[0];
+  const newDiv = createDiv({
+    id: 'profile-bio',
+    innerHTML: thisCell.innerHTML,
+  });
+  setInnerHtml('', thisCell);
+  insertElement(thisCell, newDiv);
+  compressBio();
 }
