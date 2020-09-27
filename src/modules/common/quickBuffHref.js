ea916d476d164f2525c9b1a2cc975d2285e811a0
atompkins <atompkins@gmail.com>
2020-09-27 22:15:03 +0100

    refactor quickBuffHref (old style)

--- src/modules/common/quickBuffHref.js
+++
@@ -1,8 +0,0 @@
-import { quickbuffUrl } from '../support/constants';
-
-export default function quickBuffHref(aPlayerId, buffList) { // Bad Pattern
-  let passthru = '';
-  if (buffList) { passthru = `&blist=${buffList}`; }
-  return `href='javascript:window.openWindow("${quickbuffUrl}&tid=${
-    aPlayerId}${passthru}", "fsQuickBuff", 618, 1000, ",scrollbars")'`;
-}
